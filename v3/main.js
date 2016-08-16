98340f001bb34d818f4c77b3b1fc4b1e2404d373
Davide P. Cervone <dpvc@union.edu>
2016-08-16 18:11:21 -0400

    Add output to main.js, and add 'done' to HMTL output.

--- v3/main.js
+++ v3/main.js
@@ -4,3 +4,4 @@ import "mathjax/input/LegacyTeX.js";
 
 let html = MathJax.HandlerFor("<html></html>");
 html.TestMath("\\sum_{n=0}^\\infty x_n").Compile();
+console.log(JSON.stringify(html.typeset));
