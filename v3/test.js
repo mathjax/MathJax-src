a80b1366eaf0fe2c8fedbf84e9878010d3ed5128
Davide P. Cervone <dpvc@union.edu>
2017-01-02 09:08:31 -0500

    Configure System.js to load files properly from TreeJax

--- v3/test.js
+++ v3/test.js
@@ -1,6 +1,21 @@
 process.chdir(__dirname);
 system  = require("./lib/system.js");
 system.config({map: {'traceur': './lib/traceur.min.js'}});
+system.config({
+  packages: {
+    "TreeJax/lib": {
+      map: {
+        fs: "../../lib/fs.js",
+        xmldom: "../node_modules/xmldom/dom-parser.js"
+      }
+    },
+    "TreeJax/node_modules/xmldom": {
+      map: {
+        dom: "dom.js"
+      }
+    }
+  }
+});
 
 system.import('main.js')
   .then(function (mj) {mathjax = mj.MathJax})
