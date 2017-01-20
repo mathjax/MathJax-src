0dff8c61a39f27ee389f3ba62b7d48a42ce27ce2
Davide P. Cervone <dpvc@union.edu>
2017-01-20 15:49:04 -0500

    Add options support to Input and OutputJax.

--- v3/mathjax/core/InputJax.js
+++ v3/mathjax/core/InputJax.js
@@ -1,8 +1,17 @@
+import {UserOptions, DefaultOptions} from '../util/Options.js';
+
+//
+//  The default options for InputJax
+//  
+const OPTIONS = {
+  //  (none just yet).
+};
+
 export class InputJax {
   
   constructor(options = {}) {
     this.name = this.constructor.NAME;
-    this.options = options;
+    this.options = UserOptions(DefaultOptions({},OPTIONS),options);
   }
   
   FindMath(node,options) {
--- v3/mathjax/core/OutputJax.js
+++ v3/mathjax/core/OutputJax.js
@@ -1,8 +1,17 @@
+import {UserOptions, DefaultOptions} from '../util/Options.js';
+
+//
+//  The default options for OutputJax
+//  
+const OPTIONS = {
+  //  (none just yet).
+};
+
 export class OutputJax {
   
   constructor(options = {}) {
     this.name = this.constructor.NAME;
-    this.options = options;
+    this.options = UserOptions(DefaultOptions({},OPTIONS),options);
   }
   
   Typeset(math,document,options) {
