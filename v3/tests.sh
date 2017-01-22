70c7b04e49a986afcf1f5534cc42777a0ba23041
zorkow <volker.sorge@gmail.com>
2017-01-22 01:20:26 +0000

    Testing for average time.

--- v3/tests.sh
+++ v3/tests.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-node load.js tests/json-tests.js
+if [ $# -eq 0 ]; then 
+    node load.js tests/json-tests.js
+    exit 0
+else
+    repeat=$1
+fi
+
+i=0
+sum=0
+while [ $i -lt $repeat ]
+do
+    sum=$(( $sum + `node load.js tests/json-tests.js | tail -1 | awk -Fm '{print $1}'`))
+    i=$(( $i + 1 ))
+done
+
+echo 'Total: ' $sum
+average=`bc -l <<< "($sum / $repeat)"`
+echo 'Average: ' $average
