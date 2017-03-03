4fa13cddf375e4b8034375dfa16aeb5508951f52
Davide P. Cervone <dpvc@union.edu>
2017-03-03 10:43:05 -0500

    Add default MmlFactory if one isn't specified.

--- v3/MmlTree/ts/MmlVisitor.ts
+++ v3/MmlTree/ts/MmlVisitor.ts
@@ -1,8 +1,13 @@
 import {TextNode} from './Node';
 import {XMLNode} from './MmlNode';
+import {MmlFactory} from './MmlFactory';
 import {Visitor} from './Visitor';
 
 export class MmlVisitor extends Visitor {
+    constructor(factory: MmlFactory = null) {
+        if (!factory) factory = new MmlFactory();
+        super(factory);
+    }
     visitTextNode(node: TextNode, ...args: any[]): any {}
     visitXmlNode(node: XMLNode, ...args: any[]): any {}
 }
