d1a6e39f93d046d155a2ccfa48c2205e5e6a1832
Davide P. Cervone <dpvc@union.edu>
2017-04-23 08:56:00 -0400

    Convert mathjax.js to typescript

--- v3/mathjax3-ts/mathjax.ts
+++ v3/mathjax3-ts/mathjax.ts
@@ -1,14 +1,15 @@
 import {HandlerList} from "./core/HandlerList.js";
-import {HandleRetriesFor,RetryAfter} from "./util/Retries.js";
+import {HandleRetriesFor, RetryAfter} from "./util/Retries.js";
+import {OptionList} from "./util/Options.js";
 
 export const MathJax = {
-  version: "3.0.0",
-  handlers: new HandlerList(),
-  
-  HandlerFor: function (document,options) {
-    return this.handlers.HandlerFor(document,options);
-  },
-  
-  HandleRetriesFor: HandleRetriesFor,
-  RetryAfter: RetryAfter
+    version: "3.0.0",
+    handlers: new HandlerList(),
+
+    Document: function (document: any, options: OptionList) {
+        return this.handlers.Document(document,options);
+    },
+
+    HandleRetriesFor: HandleRetriesFor,
+    RetryAfter: RetryAfter
 };
