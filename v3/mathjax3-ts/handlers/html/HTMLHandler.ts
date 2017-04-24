b816d43099495c73d6ca7b812c47847b36734701
Davide P. Cervone <dpvc@union.edu>
2017-04-24 16:39:15 -0400

    Make HTMLHandler process HTMLElement and DocumentFragment objects

--- v3/mathjax3-ts/handlers/html/HTMLHandler.ts
+++ v3/mathjax3-ts/handlers/html/HTMLHandler.ts
@@ -22,16 +22,24 @@ export class HTMLHandler extends AbstractHandler {
         if (typeof(document) === "string") {
             try {document = parser.parseFromString(document, 'text/html')} catch (err) {}
         }
-        if (document instanceof DOCUMENT) return true;
-        if (document instanceof HTMLELEMENT) return true;
-        if (document instanceof FRAGMENT) return true;
+        if (document instanceof DOCUMENT ||
+            document instanceof HTMLELEMENT ||
+            document instanceof FRAGMENT) return true;
         return false;
     }
 
     Create(document: any, options: OptionList) {
         if (typeof(document) === "string") {
             document = parser.parseFromString(document, 'text/html');
+        } else if (document instanceof HTMLELEMENT) {
+            let child = document;
+            document = parser.parseFromString('', 'text/html');
+            document.body.appendChild(child);
+        } else if (document instanceof FRAGMENT) {
+            let fragment = document;
+            document = parser.parseFromString('', 'text/html');
+            document.body.appendChild(fragment);
         }
-        return new HTMLDocument(document,options);
+        return new HTMLDocument(document, options);
     }
 };
