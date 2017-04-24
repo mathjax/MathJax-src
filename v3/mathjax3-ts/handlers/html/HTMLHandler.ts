8c963c8df3d7414cfcc681322cd8d6049d8568fe
Davide P. Cervone <dpvc@union.edu>
2017-04-24 16:09:52 -0400

    Handle DOMParser and window elements in a way Typescript can process

--- v3/mathjax3-ts/handlers/html/HTMLHandler.ts
+++ v3/mathjax3-ts/handlers/html/HTMLHandler.ts
@@ -1,15 +1,19 @@
 import {AbstractHandler} from "../../core/Handler.js";
 import {HTMLDocument} from "./HTMLDocument.js";
 import {OptionList} from "../../util/Options.js";
-import {document, window} from "../../util/document.js";
+import {document, window, DOMParser} from "../../util/document.js";
 
 const VERSION = "3.0.0";
 
+interface DOMWindow {
+    HTMLElement?: typeof HTMLElement;
+    DocumentFragment?: typeof DocumentFragment;
+}
 
 const parser = new DOMParser();
 const DOCUMENT = document.constructor;
-const HTMLELEMENT = HTMLElement;
-const FRAGMENT = DocumentFragment;
+const HTMLELEMENT = (window as DOMWindow).HTMLElement;
+const FRAGMENT = (window as DOMWindow).DocumentFragment;
 
 export class HTMLHandler extends AbstractHandler {
     public static version = VERSION;
