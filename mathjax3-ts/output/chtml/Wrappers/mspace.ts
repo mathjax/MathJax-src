f6a2cb76795d0d24e3c17cb6fc35543abc7bcded
Davide P. Cervone <dpvc@union.edu>
2017-08-22 12:00:20 -0400

    Use standard CHTML to create node (handles href's, color, attributes, etc.)

--- mathjax3-ts/output/chtml/Wrappers/mspace.ts
+++ mathjax3-ts/output/chtml/Wrappers/mspace.ts
@@ -38,12 +38,7 @@ export class CHTMLmspace extends CHTMLWrapper {
      * @override
      */
     public toCHTML(parent: HTMLElement) {
-        let chtml = this.html('mjx-mspace');
-        this.chtml = parent.appendChild(chtml);
-        this.handleScale();
-        this.handleColor();
-        this.handleSpace();
-        this.handleAttributes();
+        let chtml = this.standardCHTMLnode(parent);
         let {w, h, d} = this.getBBox();
         if (w < 0) {
             chtml.style.marginRight = this.em(w);
