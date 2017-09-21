39482a0466fd92010feff8123690519f167b2f94
zorkow <volker.sorge@gmail.com>
2017-09-21 16:17:47 +0100

    Adds tests for error states.

--- tests/parser-base-tests.js
+++ tests/parser-base-tests.js
@@ -5713,4 +5713,206 @@ parserTest.runTest(
       "isInferred":true}]}
 );
 
+// Errors
+// Ampersand error
+parserTest.ignoreTest(
+  'Ampersand-error', '&',
+  {"kind":"math",
+   "texClass":0,
+   "attributes":{"display":"block"},
+   "inherited":{"displaystyle":false,
+                "scriptlevel":0},
+   "properties":{},
+   "childNodes":[
+     {"kind":"mrow",
+      "texClass":0,
+      "attributes":{},
+      "inherited":{"display":"block",
+                   "displaystyle":true,
+                   "scriptlevel":0},
+      "properties":{},
+      "childNodes":[
+        {"kind":"merror",
+         "texClass":0,
+         "attributes":{},
+         "inherited":{"display":"block",
+                      "displaystyle":true,
+                      "scriptlevel":0},
+         "properties":{},
+         "childNodes":[
+           {"kind":"mrow",
+            "texClass":null,
+            "attributes":{},
+            "inherited":{"display":"block",
+                         "displaystyle":true,
+                         "scriptlevel":0},
+            "properties":{},
+            "childNodes":[
+              {"kind":"mtext",
+               "texClass":0,
+               "attributes":{},
+               "inherited":{"display":"block",
+                            "displaystyle":true,
+                            "scriptlevel":0},
+               "properties":{},
+               "childNodes":[
+                 {"kind":"text",
+                  "text":"Misplaced &"}],
+               "isSpacelike":true}],
+            "isInferred":true,
+            "isSpacelike":true}]}],
+      "isInferred":true}]}
+);
+
+// Argument error
+parserTest.runTest(
+  'Argument-error', '\\frac{b}',
+  {"kind":"math",
+   "texClass":0,
+   "attributes":{"display":"block"},
+   "inherited":{"displaystyle":false,
+                "scriptlevel":0},
+   "properties":{},
+   "childNodes":[
+     {"kind":"mrow",
+      "texClass":0,
+      "attributes":{},
+      "inherited":{"display":"block",
+                   "displaystyle":true,
+                   "scriptlevel":0},
+      "properties":{},
+      "childNodes":[
+        {"kind":"merror",
+         "texClass":0,
+         "attributes":{},
+         "inherited":{"display":"block",
+                      "displaystyle":true,
+                      "scriptlevel":0},
+         "properties":{},
+         "childNodes":[
+           {"kind":"mrow",
+            "texClass":null,
+            "attributes":{},
+            "inherited":{"display":"block",
+                         "displaystyle":true,
+                         "scriptlevel":0},
+            "properties":{},
+            "childNodes":[
+              {"kind":"mtext",
+               "texClass":0,
+               "attributes":{},
+               "inherited":{"display":"block",
+                            "displaystyle":true,
+                            "scriptlevel":0},
+               "properties":{},
+               "childNodes":[
+                 {"kind":"text",
+                  "text":"Missing argument for \\frac"}],
+               "isSpacelike":true}],
+            "isInferred":true,
+            "isSpacelike":true}]}],
+      "isInferred":true}]}
+);
+
+// Control sequence error
+parserTest.runTest(
+  'Undefined-CS', '\\nonsense',
+  {"kind":"math",
+   "texClass":0,
+   "attributes":{"display":"block"},
+   "inherited":{"displaystyle":false,
+                "scriptlevel":0},
+   "properties":{},
+   "childNodes":[
+     {"kind":"mrow",
+      "texClass":0,
+      "attributes":{},
+      "inherited":{"display":"block",
+                   "displaystyle":true,
+                   "scriptlevel":0},
+      "properties":{},
+      "childNodes":[
+        {"kind":"merror",
+         "texClass":0,
+         "attributes":{},
+         "inherited":{"display":"block",
+                      "displaystyle":true,
+                      "scriptlevel":0},
+         "properties":{},
+         "childNodes":[
+           {"kind":"mrow",
+            "texClass":null,
+            "attributes":{},
+            "inherited":{"display":"block",
+                         "displaystyle":true,
+                         "scriptlevel":0},
+            "properties":{},
+            "childNodes":[
+              {"kind":"mtext",
+               "texClass":0,
+               "attributes":{},
+               "inherited":{"display":"block",
+                            "displaystyle":true,
+                            "scriptlevel":0},
+               "properties":{},
+               "childNodes":[
+                 {"kind":"text",
+                  "text":"Undefined control sequence \\nonsense"}],
+               "isSpacelike":true}],
+            "isInferred":true,
+            "isSpacelike":true}]}],
+      "isInferred":true}]}
+);
+
+// Environment sequence error
+parserTest.runTest(
+  'Undefined-Env', '\\begin{nonsense} a \\end{nonsense}',
+  {"kind":"math",
+   "texClass":0,
+   "attributes":{"display":"block"},
+   "inherited":{"displaystyle":false,
+                "scriptlevel":0},
+   "properties":{},
+   "childNodes":[
+     {"kind":"mrow",
+      "texClass":0,
+      "attributes":{},
+      "inherited":{"display":"block",
+                   "displaystyle":true,
+                   "scriptlevel":0},
+      "properties":{},
+      "childNodes":[
+        {"kind":"merror",
+         "texClass":0,
+         "attributes":{},
+         "inherited":{"display":"block",
+                      "displaystyle":true,
+                      "scriptlevel":0},
+         "properties":{},
+         "childNodes":[
+           {"kind":"mrow",
+            "texClass":null,
+            "attributes":{},
+            "inherited":{"display":"block",
+                         "displaystyle":true,
+                         "scriptlevel":0},
+            "properties":{},
+            "childNodes":[
+              {"kind":"mtext",
+               "texClass":0,
+               "attributes":{},
+               "inherited":{"display":"block",
+                            "displaystyle":true,
+                            "scriptlevel":0},
+               "properties":{},
+               "childNodes":[
+                 {"kind":"text",
+                  "text":"Unknown environment 'nonsense'"}],
+               "isSpacelike":true}],
+            "isInferred":true,
+            "isSpacelike":true}]}],
+      "isInferred":true}]}
+);
+
+
 parserTest.printTime();
