4f92b461d4648f39149ca64662fb65217992aca5
zorkow <volker.sorge@gmail.com>
2017-09-21 16:40:43 +0100

    Splits up tests into more categories and files.

--- tests.sh
+++ tests.sh
@@ -4,6 +4,8 @@ if [ $# -eq 0 ]; then
     # node load.js tests/json-tests.js
     # node load.js tests/tree-tests.js
     node load.js tests/parser-base-tests.js
+    node load.js tests/parser-error-tests.js
+    node load.js tests/parser-complex-tests.js
     node load.js tests/parser-ams-tests.js
     exit 0
 else
