908312562eb71f4b2460d795584bd49ff4d79a7d
Davide P. Cervone <dpvc@union.edu>
2018-09-27 16:08:17 -0400

    Fix problem with change of case in entities array, and export it so Volker can use it.

--- mathjax3-ts/util/Entities.ts
+++ mathjax3-ts/util/Entities.ts
@@ -42,7 +42,7 @@ export const options: OptionList = {
  *  The entity name-to-value translation table
  *  (basic math entities -- others are loaded from external files)
  */
-const entities: EntityList = {
+export const entities: EntityList = {
     ApplyFunction: '\u2061',
     Backslash: '\u2216',
     Because: '\u2235',
@@ -459,8 +459,8 @@ const loaded: {[name: string]: boolean} = {};
  * @param {EntityList} entities  The entities to add
  * @param {string} file          The name of the file that they came from
  */
-export function add(entities: EntityList, file: string) {
-    Object.assign(entities, entities);
+export function add(additions: EntityList, file: string) {
+    Object.assign(entities, additions);
    loaded[file] = true;
 }
 
