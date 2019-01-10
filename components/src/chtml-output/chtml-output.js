d2c2a61ea5fc1d2f9da84070ae8ba9d3c04882d2
Davide P. Cervone <dpvc@union.edu>
2019-01-10 11:10:19 -0500

    Include global.js into core library

--- components/src/chtml-output/chtml-output.js
+++ components/src/chtml-output/chtml-output.js
@@ -1,7 +1,7 @@
-const combineDefaults = require('../../../mathjax3/components/global.js').combineDefaults;
 require('./lib/chtml-output.js');
 
 if (MathJax.loader) {
+    const combineDefaults = require('../../../mathjax3/components/global.js').combineDefaults;
     combineDefaults(MathJax.config.loader, 'chtml-output', {
         checkReady() {
             return MathJax.loader.load("chtml-fonts/tex");
