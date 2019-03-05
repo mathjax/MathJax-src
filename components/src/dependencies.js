8200230c0c800496302a3ff9b2439352e1de832c
Davide P. Cervone <dpvc@union.edu>
2019-03-05 15:43:18 -0500

    Move dependencies to a common file and incliude them in loader and startup

---
+++ components/src/dependencies.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, '__esModule', {value: true});
+exports.dependencies = {
+    'a11y/semantic-enrich': ['core', 'sre', 'mml-input'],
+    'a11y/complexity': ['core', 'sre', 'mml-input'],
+    'a11y/explorer': ['core', 'sre']
+};
--- components/src/loader/loader.js
+++ components/src/loader/loader.js
@@ -1,6 +1,10 @@
 import './lib/loader.js';
 
 import {Loader, CONFIG} from '../../../mathjax3/components/loader.js';
+import {combineDefaults} from '../../../mathjax3/components/global.js';
+import {dependencies} from '../dependencies.js';
+
+combineDefaults(MathJax.config, 'loader', {dependencies});
 
 Loader.load(...CONFIG.load)
     .then(() => CONFIG.ready())
