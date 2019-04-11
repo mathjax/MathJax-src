8fe0b6240307ed75bda4419ff29f334fc6f58a5d
Davide P. Cervone <dpvc@union.edu>
2019-04-11 12:22:37 -0400

    Update color extension to work with \require, and to use a color sub-block for configuration

--- mathjax3-ts/input/tex/color/ColorMethods.ts
+++ mathjax3-ts/input/tex/color/ColorMethods.ts
@@ -62,7 +62,7 @@ export const ColorMethods: Record<string, ParseMethod> = {};
 ColorMethods.Color = function (parser: TexParser, name: string) {
     const model = parser.GetBrackets(name, '');
     const colorDef = parser.GetArgument(name);
-    const colorModel: ColorModel = parser.options['colorModel'];
+    const colorModel: ColorModel = parser.options.color.model;
     const color = colorModel.getColor(model, colorDef);
 
     const style = parser.itemFactory.create('style')
@@ -82,7 +82,7 @@ ColorMethods.Color = function (parser: TexParser, name: string) {
 ColorMethods.TextColor = function (parser: TexParser, name: string) {
     const model = parser.GetBrackets(name, '');
     const colorDef = parser.GetArgument(name);
-    const colorModel: ColorModel = parser.options['colorModel'];
+    const colorModel: ColorModel = parser.options.color.model;
     const color = colorModel.getColor(model, colorDef);
     const old = parser.stack.env['color'];
 
@@ -110,7 +110,7 @@ ColorMethods.DefineColor = function (parser: TexParser, name: string) {
     const model = parser.GetArgument(name);
     const def = parser.GetArgument(name);
 
-    const colorModel: ColorModel = parser.options['colorModel'];
+    const colorModel: ColorModel = parser.options.color.model;
     colorModel.defineColor(model, cname, def);
 };
 
@@ -123,13 +123,14 @@ ColorMethods.DefineColor = function (parser: TexParser, name: string) {
 ColorMethods.ColorBox = function (parser: TexParser, name: string) {
     const cname = parser.GetArgument(name);
     const math = ParseUtil.internalMath(parser, parser.GetArgument(name));
-    const colorModel: ColorModel = parser.options['colorModel'];
+    const options = parser.options.color;
+    const colorModel: ColorModel = options.model;
 
     const node = parser.create('node', 'mpadded', math, {
         mathbackground: colorModel.getColor('named', cname)
     });
 
-    NodeUtil.setProperties(node, padding(parser.options['colorPadding']));
+    NodeUtil.setProperties(node, padding(options.padding));
     parser.Push(node);
 };
 
@@ -143,13 +144,14 @@ ColorMethods.FColorBox = function (parser: TexParser, name: string) {
     const fname = parser.GetArgument(name);
     const cname = parser.GetArgument(name);
     const math = ParseUtil.internalMath(parser, parser.GetArgument(name));
-    const colorModel: ColorModel = parser.options['colorModel'];
+    const options = parser.options.color;
+    const colorModel: ColorModel = options.model;
 
     const node = parser.create('node', 'mpadded', math, {
         mathbackground: colorModel.getColor('named', cname),
-        style: `border: ${parser.options['colorBorderWidth']} solid ${colorModel.getColor('named', fname)}`
+        style: `border: ${options.borderWidth} solid ${colorModel.getColor('named', fname)}`
     });
 
-    NodeUtil.setProperties(node, padding(parser.options['colorPadding']));
+    NodeUtil.setProperties(node, padding(options.padding));
     parser.Push(node);
 };
