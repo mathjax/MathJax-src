9a825faa845a9398010d59f384f2da1dd621fe4a
Davide P. Cervone <dpvc@union.edu>
2019-04-12 11:16:14 -0400

    Only add menu to handler chain if we are in the browser

--- components/src/ui/menu/menu.js
+++ components/src/ui/menu/menu.js
@@ -2,7 +2,7 @@ import './lib/menu.js';
 
 import {MenuHandler} from '../../../../mathjax3/ui/menu/MenuHandler.js';
 
-if (MathJax.startup) {
+if (MathJax.startup && typeof window !== 'undefined') {
     MathJax.startup.extendHandler(handler => MenuHandler(handler), 20);
 }
 
