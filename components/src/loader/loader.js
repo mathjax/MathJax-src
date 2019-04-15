4c342fc76ff2ad2e5b78ce8090b547a961e5dad5
Davide P. Cervone <dpvc@union.edu>
2019-04-15 10:43:38 -0400

    Add paths and provides in dependencies and update loader and startup to use them.  Add data for tex extensions

--- components/src/loader/loader.js
+++ components/src/loader/loader.js
@@ -2,9 +2,11 @@ import './lib/loader.js';
 
 import {Loader, CONFIG} from '../../../mathjax3/components/loader.js';
 import {combineDefaults} from '../../../mathjax3/components/global.js';
-import {dependencies} from '../dependencies.js';
+import {dependencies, paths, provides} from '../dependencies.js';
 
 combineDefaults(MathJax.config.loader, 'dependencies', dependencies);
+combineDefaults(MathJax.config.loader, 'paths', paths);
+combineDefaults(MathJax.config.loader, 'provides', provides);
 
 Loader.load(...CONFIG.load)
     .then(() => CONFIG.ready())
