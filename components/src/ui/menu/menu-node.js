e700676e0cf9ff85df570e554175703f4f735817
Davide P. Cervone <dpvc@union.edu>
2019-04-27 18:18:17 -0400

    Make versions of menu.js for node and for preloading the context-menu code

---
+++ components/src/ui/menu/menu-node.js
@@ -0,0 +1,9 @@
+require('context-menu');
+require('./lib/menu.js');
+
+const {MenuHandler} = require('../../../../mathjax3/ui/menu/MenuHandler.js');
+
+if (MathJax.startup && typeof window !== 'undefined') {
+    MathJax.startup.extendHandler(handler => MenuHandler(handler), 20);
+}
+
