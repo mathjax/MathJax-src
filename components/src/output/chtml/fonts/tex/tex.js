9eb0c3a7d39800765c08290e26e798e68184fd08
Davide P. Cervone <dpvc@union.edu>
2019-04-30 07:38:51 -0400

    Move woff fonts to mathjax3-ts directory (last holdout in mathjax2)

--- components/src/output/chtml/fonts/tex/tex.js
+++ components/src/output/chtml/fonts/tex/tex.js
@@ -7,7 +7,7 @@ import {TeXFont} from '../../../../../../mathjax3/output/chtml/fonts/tex.js';
 
 if (MathJax.startup) {
     combineDefaults(MathJax.config, 'chtml', {
-        fontURL: Package.resolvePath('output/chtml/fonts/tex-v2', false)
+        fontURL: Package.resolvePath('output/chtml/fonts/woff-v2', false)
     });
     const options = selectOptionsFromKeys(MathJax.config.chtml || {}, TeXFont.OPTIONS);
     combineDefaults(MathJax.config, 'chtml', {font: new TeXFont(options)});
--- mathjax3-ts/output/chtml/FontData.ts
+++ mathjax3-ts/output/chtml/FontData.ts
@@ -75,7 +75,7 @@ export class CHTMLFontData extends FontData<CHTMLCharOptions, CHTMLVariantData, 
      * Default options
      */
     public static OPTIONS = {
-        fontURL: 'mathjax2/css/woff'
+        fontURL: 'mathjax23-ts/output/chtml/fonts/tex-woff'
     };
 
     /**
