e1e5ea45f4ffe7fe4d79a73b4dc479420f8a3ba8
zorkow <volker.sorge@gmail.com>
2019-05-04 18:02:34 +0100

    Rewrites some TeX sample files to remove TestMath.

--- samples/tex-json.js
+++ samples/tex-json.js
@@ -3,6 +3,7 @@ import {MathJax} from '../mathjax3/mathjax.js';
 import {TeX} from '../mathjax3/input/tex.js';
 import {RegisterHTMLHandler} from '../mathjax3/handlers/html.js';
 import {chooseAdaptor} from '../mathjax3/adaptors/chooseAdaptor.js';
+import {STATE} from '../mathjax3/core/MathItem.js';
 import '../mathjax3/input/tex/base/BaseConfiguration.js';
 import '../mathjax3/input/tex/ams/AmsConfiguration.js';
 import '../mathjax3/input/tex/noundefined/NoUndefinedConfiguration.js';
@@ -17,15 +18,13 @@ let html = MathJax.document('<html></html>', {
   InputJax: new TeX({packages: ['base', 'ams', 'boldsymbol', 'newcommand', 'mhchem', 'braket']})
 });
 
-console.log(html.inputJax[0].configuration);
 import {JsonMmlVisitor} from '../mathjax3/core/MmlTree/JsonMmlVisitor.js';
 let visitor = new JsonMmlVisitor();
 let toJSON = (node => visitor.visitTree(node));
 
 MathJax.handleRetriesFor(() => {
 
-    html.TestMath(process.argv[3] || '').compile();
-    let math = html.math.pop().root;
+    let math = html.convert(process.argv[3] || '', {end: STATE.CONVERT});
     math.setTeXclass();
     console.log(JSON.stringify(toJSON(math)));
 
--- samples/tex2mml.js
+++ samples/tex2mml.js
@@ -3,6 +3,7 @@ import {MathJax} from '../mathjax3/mathjax.js';
 import {TeX} from '../mathjax3/input/tex.js';
 import {RegisterHTMLHandler} from '../mathjax3/handlers/html.js';
 import {chooseAdaptor} from '../mathjax3/adaptors/chooseAdaptor.js';
+import {STATE} from '../mathjax3/core/MathItem.js';
 import '../mathjax3/input/tex/base/BaseConfiguration.js';
 import '../mathjax3/input/tex/ams/AmsConfiguration.js';
 import '../mathjax3/input/tex/noundefined/NoUndefinedConfiguration.js';
@@ -16,15 +17,13 @@ let html = MathJax.document('<html></html>', {
   InputJax: new TeX({packages: ['base', 'ams', 'boldsymbol', 'newcommand', 'braket']})
 });
 
-// import {TestMmlVisitor as MmlVisitor} from '../mathjax3/core/MmlTree/TestMmlVisitor.js';
 import {SerializedMmlVisitor as MmlVisitor} from '../mathjax3/core/MmlTree/SerializedMmlVisitor.js';
 let visitor = new MmlVisitor();
 let toMml = (node => visitor.visitTree(node, html.document));
 
 MathJax.handleRetriesFor(() => {
 
-    html.TestMath(process.argv[3] || '').compile();
-    let math = html.math.pop().root;
+    let math = html.convert(process.argv[3] || '', {end: STATE.CONVERT});
     math.setTeXclass();
     console.log(toMml(math));
 
