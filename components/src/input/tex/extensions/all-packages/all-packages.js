d3e9dd30380068936429abd7689182a4e153907a
Davide P. Cervone <dpvc@union.edu>
2019-05-14 11:21:25 -0400

    Remove require from AllPackages (since it forces loading of loader/startup code), and put it into the tex-full and all-packages components directly.  Remove physics from the AllPackages list, since it conflicts with standard definitions

--- components/src/input/tex/extensions/all-packages/all-packages.js
+++ components/src/input/tex/extensions/all-packages/all-packages.js
@@ -1,6 +1,7 @@
 import './lib/all-packages.js';
 
 import {AllPackages} from '../../../../../../mathjax3/input/tex/AllPackages.js';
+import '../../../../../../mathjax3/input/tex/require/RequireConfiguration.js';
 import {insert} from '../../../../../../mathjax3/util/Options.js';
 
 if (MathJax.startup) {
@@ -8,7 +9,7 @@ if (MathJax.startup) {
         MathJax.config.tex = {};
     }
     let packages = MathJax.config.tex.packages;
-    MathJax.config.tex.packages = [...AllPackages];
+    MathJax.config.tex.packages = ['require',...AllPackages];
     if (packages) {
         insert(MathJax.config.tex, {packages});
     }
