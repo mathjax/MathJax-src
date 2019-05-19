de6e42f15f207a69ebf57c163e456a11df56d347
Davide P. Cervone <dpvc@union.edu>
2019-05-19 08:55:10 -0400

    Change version to 3.0.0-beta.4

--- mathjax3-ts/mathjax.ts
+++ mathjax3-ts/mathjax.ts
@@ -34,7 +34,7 @@ export const MathJax = {
     /**
      *  The MathJax version number
      */
-    version: '3.0.0',
+    version: '3.0.0-beta.4',
 
     /**
      *  The list of registers document handlers
