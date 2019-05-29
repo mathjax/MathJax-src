3dd7c6cf3a8272c6b4c24b08a610f64c5c48b9aa
zorkow <volker.sorge@gmail.com>
2019-05-29 19:59:55 +0100

    Linting package file.

--- mathjax3-ts/input/tex/verb/VerbConfiguration.ts
+++ mathjax3-ts/input/tex/verb/VerbConfiguration.ts
@@ -45,12 +45,14 @@ VerbMethods.Verb = function(parser: TexParser, name: string, notation: string) {
   if (c === '' ) {
     throw new TexError('MissingArgFor', 'Missing argument for %1', name);
   }
-  while (parser.i < parser.string.length && parser.string.charAt(parser.i) !== c) {
+  while (parser.i < parser.string.length &&
+         parser.string.charAt(parser.i) !== c) {
     parser.i++;
   }
   if (parser.i === parser.string.length) {
     throw new TexError('NoClosingDelim',
-                       'Can\'t find closing delimiter for %1', parser.currentCS);
+                       'Can\'t find closing delimiter for %1',
+                       parser.currentCS);
   }
   const text = parser.string.slice(start, parser.i).replace(/ /g, '\u00A0');
   parser.i++;
