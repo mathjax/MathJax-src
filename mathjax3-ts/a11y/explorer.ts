3c1b7785b1c289593fc8402374d429be8616387f
zorkow <volker.sorge@gmail.com>
2019-06-18 15:57:37 +0100

    Removes unused imports.

--- mathjax3-ts/a11y/explorer.ts
+++ mathjax3-ts/a11y/explorer.ts
@@ -31,7 +31,7 @@ import {OptionList, expandable} from '../util/Options.js';
 import {BitField} from '../util/BitField.js';
 import {SerializedMmlVisitor} from '../core/MmlTree/SerializedMmlVisitor.js';
 
-import {ContentHoverer, EffectHoverer, ValueHoverer, AbstractKeyExplorer, Magnifier, Explorer, SpeechExplorer} from './explorer/Explorer.js';
+import {EffectHoverer, AbstractKeyExplorer, Magnifier, Explorer, SpeechExplorer} from './explorer/Explorer.js';
 import {LiveRegion, ToolTip, HoverRegion} from './explorer/Region.js';
 
 /**
