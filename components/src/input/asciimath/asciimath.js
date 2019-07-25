18ed8e38b8ba2cffc739e8a6d67ca923cc7a68fa
Davide P. Cervone <dpvc@union.edu>
2019-07-25 09:31:00 -0400

    Allow standard component configuration for AsciiMath

--- components/src/input/asciimath/asciimath.js
+++ components/src/input/asciimath/asciimath.js
@@ -3,6 +3,7 @@ import './lib/asciimath.js';
 import {AsciiMath} from '../../../../mathjax3/input/asciimath.js';
 
 if (MathJax.startup) {
+    MathJax.Hub.Config({AsciiMath: MathJax.config.asciimath || {}});
     MathJax.startup.registerConstructor('asciimath', AsciiMath);
     MathJax.startup.useInput('asciimath');
 }
