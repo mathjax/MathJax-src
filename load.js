1479a0e10b00fde17c8a9e23339d9aa28a8432f4
Davide P. Cervone <dpvc@union.edu>
2019-08-28 09:27:40 -0400

    Remove developer files not needed in source distribution

--- load.js
+++
@@ -1,8 +0,0 @@
-process.chdir(__dirname);
-require("./lib/system.js");
-System.nodeRequire = require;  // make this available to modules running in node for now
-System.config({map: {'traceur': './lib/traceur.min.js'}});
-
-System.import(process.argv[2] || 'main.js')
-  .then(function (mj) {mathjax = mj.MathJax})
-  .catch(function (error) {console.log(error.message)});
