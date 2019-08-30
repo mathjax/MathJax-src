35c797cc1a2c7f156e9ce8171236911aba249d7c
Davide P. Cervone <dpvc@union.edu>
2019-08-30 19:44:17 -0400

    Handle STATE so that we don't need to load MathItem.js

--- components/src/startup/webpack.config.js
+++ components/src/startup/webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require("webpack");
 const PACKAGE = require('../../webpack.common.js');
 
 module.exports = PACKAGE(
@@ -6,18 +5,4 @@ module.exports = PACKAGE(
     '../../../js',                      // location of the MathJax js library
     [],                                 // packages to link to
     __dirname                           // our directory
-);
-
-//
-//  Force linking to core/MathItem.js from the core package, since
-//  that is not needed until after core is loaded (can't link to all
-//  of core, as we need PrioritizedList.js and global.js before that)
-//
-module.exports.plugins.push(
-    new webpack.NormalModuleReplacementPlugin(
-        /\/core\/MathItem\.js$/,
-        function (resource) {
-            resource.request = '../../components/src/core/lib/core/MathItem.js';
-        }
-    )
 );
