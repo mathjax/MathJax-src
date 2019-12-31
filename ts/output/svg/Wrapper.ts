5d1db2c2d0d90c6108340be880ca6c414ff353fb
Davide P. Cervone <dpvc@union.edu>
2019-12-31 14:30:22 -0500

    Handle xlink namespace in all adaptors, and in the SVG wrapper.  Resolves issue #365.

--- ts/output/svg/Wrapper.ts
+++ ts/output/svg/Wrapper.ts
@@ -319,7 +319,7 @@ CommonWrapper<
     protected useNode(variant: string, C: string, path: string) {
         const use = this.svg('use');
         const id = '#' + this.jax.fontCache.cachePath(variant, C, path);
-        this.adaptor.setAttribute(use, 'xlink:href', id, XLINKNS);
+        this.adaptor.setAttribute(use, 'href', id, XLINKNS);
         return use;
     }
 
