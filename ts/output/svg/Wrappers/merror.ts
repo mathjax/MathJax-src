8aa28c58f861932927a29d83d956efa9f8847eab
Davide P. Cervone <dpvc@union.edu>
2020-06-16 08:31:22 -0400

    Put title element in the merror group rather than the rectangle

--- ts/output/svg/Wrappers/merror.ts
+++ ts/output/svg/Wrappers/merror.ts
@@ -60,13 +60,13 @@ export class SVGmerror<N, T, D> extends SVGWrapper<N, T, D> {
   public toSVG(parent: N) {
     const svg = this.standardSVGnode(parent);
     const {h, d, w} = this.getBBox();
-    const rect = this.adaptor.append(this.element, this.svg('rect', {
+    this.adaptor.append(this.element, this.svg('rect', {
       'data-background': true,
       width: this.fixed(w), height: this.fixed(h + d), y: this.fixed(-d)
     }));
     const title = this.node.attributes.get('title') as string;
     if (title) {
-      this.adaptor.append(rect, this.svg('title', {}, [this.adaptor.text(title)]));
+      this.adaptor.append(this.element, this.svg('title', {}, [this.adaptor.text(title)]));
     }
     this.addChildren(svg);
   }
