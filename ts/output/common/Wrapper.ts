4b7cbdc65f358114863044b2ff1fd25de26c9658
Davide P. Cervone <dpvc@union.edu>
2021-05-24 11:53:01 -0400

    Use ?. operation rather than && test, as per review

--- ts/output/common/Wrapper.ts
+++ ts/output/common/Wrapper.ts
@@ -375,12 +375,14 @@ export class CommonWrapper<
    */
   protected copySkewIC(bbox: BBox) {
     const first = this.childNodes[0];
-    if (first) {
-      first.bbox.sk && (bbox.sk = first.bbox.sk);
-      first.bbox.dx && (bbox.dx = first.bbox.dx);
+    if (first?.bbox.sk) {
+      bbox.sk = first.bbox.sk;
+    }
+    if (first?.bbox.dx) {
+      bbox.dx = first.bbox.dx;
     }
     const last = this.childNodes[this.childNodes.length - 1];
-    if (last && last.bbox.ic) {
+    if (last?.bbox.ic) {
       bbox.ic = last.bbox.ic;
       bbox.w += bbox.ic;
     }
