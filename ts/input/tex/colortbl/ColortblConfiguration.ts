cbff6ea48ce8b301726538ac9ce29a3a53076cef
Davide P. Cervone <dpvc@union.edu>
2021-06-01 15:23:55 -0400

    Make changes as per code review

--- ts/input/tex/colortbl/ColortblConfiguration.ts
+++ ts/input/tex/colortbl/ColortblConfiguration.ts
@@ -1,3 +1,27 @@
+/*************************************************************
+ *
+ *  Copyright (c) 2021 The MathJax Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+/**
+ * @fileoverview    Configuration file for the colortbl package.
+ *
+ * @author dpvc@mathjax.org (Davide P. Cervone)
+ */
+
 import {ArrayItem} from '../base/BaseItems.js';
 import {Configuration, ParserConfiguration, ConfigurationHandler} from '../Configuration.js';
 import {CommandMap} from '../SymbolMap.js';
@@ -7,11 +31,14 @@ import {MmlNode} from '../../../core/MmlTree/MmlNode.js';
 
 import {TeX} from '../../tex.js';
 
-export type ColorData = {
+/**
+ * Information about table colors.
+ */
+export interface ColorData {
   cell: string;
   row: string;
   col: string[];
-};
+}
 
 //
 //  Sublcass the ArrayItem to handle colored entries
