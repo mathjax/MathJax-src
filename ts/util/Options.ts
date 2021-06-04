20af442a752c26a7fd48dc801018fdc54a236d71
Davide P. Cervone <dpvc@union.edu>
2021-06-04 12:35:21 -0400

    Provide switch for warn/error for invalid options, and configure it through startup.

--- ts/util/Options.ts
+++ ts/util/Options.ts
@@ -68,6 +68,27 @@ export const APPEND = '[+]';
  */
 export const REMOVE = '[-]';
 
+
+/**
+ *  Provides options for the option utlities.
+ */
+export const OPTIONS = {
+  invalidOption: 'warn' as ('fatal' | 'warn'),
+  /**
+   * Function to report messages for invalid options
+   *
+   * @param {string} message   The message for the invalid parameter.
+   * @param {string} key       The invalid key itself.
+   */
+  optionError: (message: string, _key: string) => {
+    if (OPTIONS.invalidOption === 'fatal') {
+      throw new Error(message);
+    }
+    console.warn('MathJax: ' + message);
+  }
+};
+
+
 /**
  * A Class to use for options that should not produce warnings if an undefined key is used
  */
@@ -163,7 +184,8 @@ export function insert(dst: OptionList, src: OptionList, warn: boolean = true): 
       if (typeof key === 'symbol') {
         key = (key as symbol).toString();
       }
-      throw new Error('Invalid option "' + key + '" (no default value).');
+      OPTIONS.optionError(`Invalid option "${key}" (no default value).`, key);
+      continue;
     }
     //
     // Shorthands for the source and destination values
