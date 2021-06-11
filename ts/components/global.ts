9c0eee73d34ad197f5e8c94596ae23b4f79adc21
Davide P. Cervone <dpvc@union.edu>
2021-06-11 08:35:16 -0400

    Update version number to 3.2

--- ts/components/global.ts
+++ ts/components/global.ts
@@ -127,7 +127,7 @@ if (typeof global.MathJax === 'undefined') {
  */
 if (!(global.MathJax as MathJaxObject).version) {
   global.MathJax = {
-    version: '3.1.4',
+    version: '3.2.0',
     _: {},
     config: global.MathJax
   };
--- ts/mathjax.ts
+++ ts/mathjax.ts
@@ -34,7 +34,7 @@ export const mathjax = {
   /**
    *  The MathJax version number
    */
-  version: '3.1.4',
+  version: '3.2.0',
 
   /**
    *  The list of registers document handlers
