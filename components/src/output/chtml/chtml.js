563f5494cc26de34963bf5108516af231272f222
Davide P. Cervone <dpvc@union.edu>
2021-08-22 09:06:26 -0400

    Move output font option to fontData and use font to specify font component to load

--- components/src/output/chtml/chtml.js
+++ components/src/output/chtml/chtml.js
@@ -6,7 +6,12 @@ import {CHTML} from '../../../../js/output/chtml.js';
 if (MathJax.loader) {
   combineDefaults(MathJax.config.loader, 'output/chtml', {
     checkReady() {
-      return MathJax.loader.load("output/chtml/fonts/tex");
+      let font = MathJax.config.svg.font || 'tex';
+      if (typeof(font) !== 'string') {
+        MathJax.config.svg.fontData = font;
+        MathJax.config.svg.font = font = 'tex';
+      }
+      return MathJax.loader.load(font.match(/^[\[\/]|^[a-z]+:/) ? font : 'output/chtml/fonts/' + font);
     }
   });
 }
--- components/src/output/svg/svg.js
+++ components/src/output/svg/svg.js
@@ -6,7 +6,12 @@ import {SVG} from '../../../../js/output/svg.js';
 if (MathJax.loader) {
   combineDefaults(MathJax.config.loader, 'output/svg', {
     checkReady() {
-      return MathJax.loader.load("output/svg/fonts/tex");
+      let font = MathJax.config.svg.font || 'tex';
+      if (typeof(font) !== 'string') {
+        MathJax.config.svg.fontData = font;
+        MathJax.config.svg.font = font = 'tex';
+      }
+      return MathJax.loader.load(font.match(/^[\[\/]|^[a-z]+:/) ? font : 'output/svg/fonts/' + font);
     }
   });
 }
