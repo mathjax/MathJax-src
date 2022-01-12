6401433ecad977185a7e9a0d01e70e4c68d43f02
Davide P. Cervone <dpvc@union.edu>
2022-01-12 07:54:58 -0500

    Take relative scaling into account for CHTML output of non-MathJax fonts (mathjax/MathJax#2818)

--- ts/output/chtml/Wrappers/TextNode.ts
+++ ts/output/chtml/Wrappers/TextNode.ts
@@ -115,7 +115,8 @@ export const ChtmlTextNode = (function <N, T, D>(): ChtmlTextNodeClass<N, T, D> 
       const text = (this.node as TextNode).getText();
       if (text.length === 0) return;
       if (variant === '-explicitFont') {
-        adaptor.append(parent, this.jax.unknownText(text, variant, this.getBBox().w));
+      const rscale = this.parent.getBBox().rscale;
+        adaptor.append(parent, this.jax.unknownText(text, variant, this.getBBox().w, rscale));
       } else {
         let utext = '';
         const chars = this.remappedText(text, variant);
