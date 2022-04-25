eee43fb6a4bd7770c1f01210cdfe3e1a8fe0ed48
zorkow <volker.sorge@gmail.com>
2022-04-25 16:13:42 +0200

    Introduces separate config component for SRE.

--- components/src/a11y/sre/sre.js
+++ components/src/a11y/sre/sre.js
@@ -1,26 +1,11 @@
 import './lib/sre.js';
-
-import {Package} from '../../../../js/components/package.js';
-
+import './sre_config.js';
 import Sre from '../../../../js/a11y/sre.js';
 
-// This sets up the correct link to the mathmaps files.
-//
-// We could also use a custom method for loading locales that are webpacked into
-// the distribution.
 if (MathJax.startup) {
-
-  const path = Package.resolvePath('[sre]', false) + '/mathmaps';
-
-  if (typeof window !== 'undefined') {
-    window.SREfeature = {json: path,
-                         custom: (loc) => Sre.preloadLocales(loc)
-                        };
-  } else {
-    // TODO: This is does not yet work correctly!
-    global.SREfeature = {json: path,
-                         // delay: true,
-                         custom: (loc) => Sre.preloadLocales(loc)
-                        };
-  }
+  console.log(3);
+  ((typeof window !== 'undefined') ? window : global).
+    SREfeature.custom = (loc) => Sre.preloadLocales(loc);
+  console.log(((typeof window !== 'undefined') ? window : global).SREfeature);
 }
+
