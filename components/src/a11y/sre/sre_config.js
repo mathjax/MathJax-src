112b44baff63de476fa352b4f8967ab0851a187c
zorkow <volker.sorge@gmail.com>
2022-04-26 11:20:24 +0200

    Cleanup components files.

--- components/src/a11y/sre/sre_config.js
+++ components/src/a11y/sre/sre_config.js
@@ -1,21 +1,13 @@
 import {Package} from '../../../../js/components/package.js';
 
 // This sets up the correct link to the mathmaps files.
-//
-// We could also use a custom method for loading locales that are webpacked into
-// the distribution.
 if (MathJax.startup) {
 
   const path = Package.resolvePath('[sre]', false) + '/mathmaps';
 
-  console.log(path);
-  console.log(0);
   if (typeof window !== 'undefined') {
-    console.log(1);
     window.SREfeature = {json: path};
   } else {
-    // TODO: This is does not yet work correctly!
-    console.log(2);
     global.SREfeature = {json: path};
   }
 }
