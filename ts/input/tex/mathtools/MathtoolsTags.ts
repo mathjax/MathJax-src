a5ae9485cb7441fdd5ea59645cfbd1c12b7d53e1
Davide P. Cervone <dpvc@union.edu>
2022-05-19 10:31:39 -0400

    Update copyright years

--- ts/input/tex/mathtools/MathtoolsTags.ts
+++ ts/input/tex/mathtools/MathtoolsTags.ts
@@ -1,5 +1,5 @@
 /*************************************************************
- *  Copyright (c) 2021-2021 MathJax Consortium
+ *  Copyright (c) 2021-2022 MathJax Consortium
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
