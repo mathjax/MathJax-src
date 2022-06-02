05ec6ad05901914f3d4d5f0858531356d562d616
Dup4 <lyuzhi.pan@gmail.com>
2022-06-02 08:53:29 +0800

    fix: add the rest of the package

--- ts/input/tex/AllPackages.ts
+++ ts/input/tex/AllPackages.ts
@@ -21,46 +21,50 @@
  * @author dpvc@mathjax.org (Davide Cervone)
  */
 
-import './base/BaseConfiguration.js';
-import './action/ActionConfiguration.js';
-import './ams/AmsConfiguration.js';
-import './amscd/AmsCdConfiguration.js';
-import './bbox/BboxConfiguration.js';
-import './boldsymbol/BoldsymbolConfiguration.js';
-import './braket/BraketConfiguration.js';
-import './bussproofs/BussproofsConfiguration.js';
-import './cancel/CancelConfiguration.js';
-import './cases/CasesConfiguration.js';
-import './centernot/CenternotConfiguration.js';
-import './color/ColorConfiguration.js';
-import './colorv2/ColorV2Configuration.js';
-import './colortbl/ColortblConfiguration.js';
-import './configmacros/ConfigMacrosConfiguration.js';
-import './empheq/EmpheqConfiguration.js';
-import './enclose/EncloseConfiguration.js';
-import './extpfeil/ExtpfeilConfiguration.js';
-import './gensymb/GensymbConfiguration.js';
-import './html/HtmlConfiguration.js';
-import './mathtools/MathtoolsConfiguration.js';
-import './mhchem/MhchemConfiguration.js';
-import './newcommand/NewcommandConfiguration.js';
-import './noerrors/NoErrorsConfiguration.js';
-import './noundefined/NoUndefinedConfiguration.js';
-import './physics/PhysicsConfiguration.js';
-import './setoptions/SetOptionsConfiguration.js';
-import './tagformat/TagFormatConfiguration.js';
-import './textcomp/TextcompConfiguration.js';
-import './textmacros/TextMacrosConfiguration.js';
-import './upgreek/UpgreekConfiguration.js';
-import './unicode/UnicodeConfiguration.js';
-import './verb/VerbConfiguration.js';
-
+ import './action/ActionConfiguration.js';
+ import './ams/AmsConfiguration.js';
+ import './amscd/AmsCdConfiguration.js';
+ import './autoload/AutoloadConfiguration.js';
+ import './base/BaseConfiguration.js';
+ import './bbox/BboxConfiguration.js';
+ import './boldsymbol/BoldsymbolConfiguration.js';
+ import './braket/BraketConfiguration.js';
+ import './bussproofs/BussproofsConfiguration.js';
+ import './cancel/CancelConfiguration.js';
+ import './cases/CasesConfiguration.js';
+ import './centernot/CenternotConfiguration.js';
+ import './color/ColorConfiguration.js';
+ import './colortbl/ColortblConfiguration.js';
+ import './colorv2/ColorV2Configuration.js';
+ import './configmacros/ConfigMacrosConfiguration.js';
+ import './empheq/EmpheqConfiguration.js';
+ import './enclose/EncloseConfiguration.js';
+ import './extpfeil/ExtpfeilConfiguration.js';
+ import './gensymb/GensymbConfiguration.js';
+ import './html/HtmlConfiguration.js';
+ import './mathtools/MathtoolsConfiguration.js';
+ import './mhchem/MhchemConfiguration.js';
+ import './newcommand/NewcommandConfiguration.js';
+ import './noerrors/NoErrorsConfiguration.js';
+ import './noundefined/NoUndefinedConfiguration.js';
+ import './physics/PhysicsConfiguration.js';
+ import './require/RequireConfiguration.js';
+ import './setoptions/SetOptionsConfiguration.js';
+ import './tagformat/TagFormatConfiguration.js';
+ import './textcomp/TextcompConfiguration.js';
+ import './textmacros/TextMacrosConfiguration.js';
+ import './unicode/UnicodeConfiguration.js';
+ import './upgreek/UpgreekConfiguration.js';
+ import './verb/VerbConfiguration.js';
+ 
 declare const MathJax: any;
 if (typeof MathJax !== 'undefined' && MathJax.loader) {
   MathJax.loader.preLoad(
+    'input/tex-base',
     '[tex]/action',
     '[tex]/ams',
     '[tex]/amscd',
+    '[tex]/autoload',
     '[tex]/bbox',
     '[tex]/boldsymbol',
     '[tex]/braket',
@@ -69,8 +73,9 @@ if (typeof MathJax !== 'undefined' && MathJax.loader) {
     '[tex]/cases',
     '[tex]/centernot',
     '[tex]/color',
-    '[tex]/colorv2',
     '[tex]/colortbl',
+    '[tex]/colorv2',
+    '[tex]/configmacros',
     '[tex]/empheq',
     '[tex]/enclose',
     '[tex]/extpfeil',
@@ -82,14 +87,14 @@ if (typeof MathJax !== 'undefined' && MathJax.loader) {
     '[tex]/noerrors',
     '[tex]/noundefined',
     '[tex]/physics',
-    '[tex]/upgreek',
-    '[tex]/unicode',
-    '[tex]/verb',
-    '[tex]/configmacros',
+    '[tex]/require',
+    '[tex]/setoptions',
     '[tex]/tagformat',
     '[tex]/textcomp',
     '[tex]/textmacros',
-    '[tex]/setoptions',
+    '[tex]/unicode',
+    '[tex]/upgreek',
+    '[tex]/verb',
   );
 }
 
@@ -98,6 +103,7 @@ export const AllPackages: string[] = [
   'action',
   'ams',
   'amscd',
+  'autoload',
   'bbox',
   'boldsymbol',
   'braket',
@@ -107,6 +113,8 @@ export const AllPackages: string[] = [
   'centernot',
   'color',
   'colortbl',
+  'colorv2',
+  'configmacros',
   'empheq',
   'enclose',
   'extpfeil',
@@ -117,11 +125,13 @@ export const AllPackages: string[] = [
   'newcommand',
   'noerrors',
   'noundefined',
-  'upgreek',
-  'unicode',
-  'verb',
-  'configmacros',
+  'physics',
+  'require',
+  'setoptions',
   'tagformat',
   'textcomp',
-  'textmacros'
+  'textmacros',
+  'unicode',
+  'upgreek',
+  'verb',
 ];
