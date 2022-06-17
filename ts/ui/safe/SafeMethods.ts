87279d2f850a38c7e9176ba375a5b324f455ac38
Sean Goedecke <sgoedecke@github.com>
2022-06-17 11:13:59 +1000

    Allow matching protocols that contain newlines
    Co-authored-by: Davide P. Cervone <dpvc33@gmail.com>

--- ts/ui/safe/SafeMethods.ts
+++ ts/ui/safe/SafeMethods.ts
@@ -41,7 +41,7 @@ export const SafeMethods: {[name: string]: FilterFunction<any, any, any>} = {
    * @template D  The Document class
    */
   filterURL<N, T, D>(safe: Safe<N, T, D>, url: string): string | null {
-    const protocol = (url.match(/^\s*([a-z\n\r]+):/i) || [null, ''])[1].toLowerCase();
+    const protocol = (url.match(/^\s*([a-z\n\r]+):/i) || [null, ''])[1].replace(/[\n\r]/g, '').toLowerCase();
     const allow = safe.allow.URLs;
     return (allow === 'all' || (allow === 'safe' &&
                                 (safe.options.safeProtocols[protocol] || !protocol))) ? url : null;
