mathjax/MathJax-src

commit d35a510f5cb27aebadb5c468e218c31aecab9c3e

Update handling of brackets in algined environments to current LaTeX behavior

ts/input/tex/ams/AmsMethods.ts
@@ -61,7 +61,7 @@ AmsMethods.AmsEqnArray = function(parser: TexParser, begin: StackItem,
                                       align: string, spacing: string,
                                       style: string) {
   // @test Aligned, Gathered
-  const args = parser.GetBrackets('\\begin{' + begin.getName() + '}');
+  const args = (parser.getCodePoint() === '[' ? parser.GetBrackets('\\begin{' + begin.getName() + '}') : '');
   const array = BaseMethods.EqnArray(parser, begin, numbered, taggable, align, spacing, style);
   return ParseUtil.setArrayAlign(array as ArrayItem, args);
 };
@@ -81,7 +81,7 @@ AmsMethods.AlignAt = function(parser: TexParser, begin: StackItem,
   let n, valign, align = '', spacing = [];
   if (!taggable) {
     // @test Alignedat
-    valign = parser.GetBrackets('\\begin{' + name + '}');
+    valign = (this.getCodePoint() === '[' ? parser.GetBrackets('\\begin{' + name + '}') : '');
   }
   n = parser.GetArgument('\\begin{' + name + '}');
   if (n.match(/[^0-9]/)) {

commit 725f128a5d7ff616c3f034f47948d6da8d6f6f80

Fix typo introduced in commit d35a510

ts/input/tex/ams/AmsMethods.ts
@@ -81,7 +81,7 @@ AmsMethods.AlignAt = function(parser: TexParser, begin: StackItem,
   let n, valign, align = '', spacing = [];
   if (!taggable) {
     // @test Alignedat
-    valign = (this.getCodePoint() === '[' ? parser.GetBrackets('\\begin{' + name + '}') : '');
+    valign = (parser.getCodePoint() === '[' ? parser.GetBrackets('\\begin{' + name + '}') : '');
   }
   n = parser.GetArgument('\\begin{' + name + '}');
   if (n.match(/[^0-9]/)) {