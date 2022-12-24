1afa4dcccf860ec510e39e885c0e8e3f465e926c
zorkow <volker.sorge@gmail.com>
2022-12-24 17:18:29 +0100

    Initial attempt at fixing over items.

--- ts/input/tex/base/BaseItems.ts
+++ ts/input/tex/base/BaseItems.ts
@@ -333,6 +333,7 @@ export class OverItem extends BaseItem {
                                    this.getProperty('ldelim') as string, mml,
                                    this.getProperty('rdelim') as string);
       }
+      mml.attributes.set('itemLatex', this.getProperty('name') as string);
       return [[this.factory.create('mml', mml), item], true];
     }
     return super.checkItem(item);
