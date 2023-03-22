73a8668622fe927b0176e47350131fcf5329dd79
Davide P. Cervone <dpvc@union.edu>
2023-03-22 13:39:09 -0400

    Don't include all of SRE in the menu component

--- ts/ui/menu/Menu.ts
+++ ts/ui/menu/Menu.ts
@@ -41,8 +41,6 @@ import {Rule} from 'mj-context-menu/js/item_rule.js';
 import {CssStyles} from 'mj-context-menu/js/css_util.js';
 import {Submenu} from 'mj-context-menu/js/item_submenu.js';
 
-import Sre from '../../a11y/sre.js';
-
 
 /*==========================================================================*/
 
@@ -431,7 +429,9 @@ export class Menu {
         this.a11yVar<boolean>('subtitles'),
         this.a11yVar<boolean>('braille'),
         this.a11yVar<boolean>('viewBraille'),
-        this.a11yVar<string>('locale', value => Sre.setupEngine({locale: value as string})),
+        this.a11yVar<string>('locale', value => {
+          MathJax._.a11y.sre.Sre.setupEngine({locale: value as string});
+        }),
         this.a11yVar<string> ('speechRules', value => {
           const [domain, style] = value.split('-');
           this.document.options.sre.domain = domain;
