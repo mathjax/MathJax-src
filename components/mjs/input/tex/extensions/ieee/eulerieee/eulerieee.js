4cdbc9479d18b7c5b1fe264bfa53235cb30252fd
zorkow <volker.sorge@gmail.com>
2024-02-03 12:48:51 +0100

    Fixes font loading in dsfont and eulerieee packages.

--- components/mjs/input/tex/extensions/ieee/eulerieee/eulerieee.js
+++ components/mjs/input/tex/extensions/ieee/eulerieee/eulerieee.js
@@ -1,1 +1,17 @@
 import './lib/eulerieee.js';
+import {MathJax, combineDefaults} from 'mathjax-full/js/components/global.js';
+
+const FONTPATH = (typeof document === 'undefined' ?
+                   '@mathjax/ieee-euler-font-extension' :
+                   'https://cdn.jsdelivr.net/npm/ieee-euler-font-extension');
+
+if (MathJax.config?.loader) {
+  combineDefaults(MathJax.config.loader, 'paths', {
+    'ieee-euler-extension': FONTPATH
+  });
+  MathJax.config.loader['[tex]/eulerieee'] = {
+    checkReady() {
+      return MathJax.loader.load(`[ieee-euler-extension]/${MathJax.config?.startup?.output || 'chtml'}`);
+    }
+  };
+}
