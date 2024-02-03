76acec1300683f670958cbc23d86c6d936bdd6a7
zorkow <volker.sorge@gmail.com>
2024-02-03 14:11:10 +0100

    Default loading of stix font for ieee stix extension.

--- components/mjs/input/tex/extensions/ieee/ieeestix/ieeestix.js
+++ components/mjs/input/tex/extensions/ieee/ieeestix/ieeestix.js
@@ -1,1 +1,17 @@
 import './lib/ieeestix.js';
+import {MathJax, combineDefaults} from 'mathjax-full/js/components/global.js';
+
+const FONTPATH = (typeof document === 'undefined' ?
+                   '@mathjax/mathjax-stix2-font' :
+                   'https://cdn.jsdelivr.net/npm/mathjax-stix2-font');
+
+if (MathJax.config?.loader) {
+  combineDefaults(MathJax.config.loader, 'paths', {
+    'mathjax-stix2': FONTPATH
+  });
+  MathJax.config.loader['[tex]/ieeestix'] = {
+    checkReady() {
+      return MathJax.loader.load(`[mathjax-stix2]/${MathJax.config?.startup?.output || 'chtml'}`);
+    }
+  };
+}
