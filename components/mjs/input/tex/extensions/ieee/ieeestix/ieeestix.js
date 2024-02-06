77d326ec81478c2970200fdab225000a7e046c59
zorkow <volker.sorge@gmail.com>
2024-02-06 13:52:52 +0100

    Use stix2 fonts as default in ieeestix.

--- components/mjs/input/tex/extensions/ieee/ieeestix/ieeestix.js
+++ components/mjs/input/tex/extensions/ieee/ieeestix/ieeestix.js
@@ -1,17 +1,31 @@
 import './lib/ieeestix.js';
 import {MathJax, combineDefaults} from 'mathjax-full/js/components/global.js';
 
+// Temporary for local testing!
+// const FONTPATH = 'node_modules/mathjax-stix2-font';
 const FONTPATH = (typeof document === 'undefined' ?
                    '@mathjax/mathjax-stix2-font' :
                    'https://cdn.jsdelivr.net/npm/mathjax-stix2-font');
 
-if (MathJax.config?.loader) {
-  combineDefaults(MathJax.config.loader, 'paths', {
+combineDefaults(MathJax.config, 'loader', {
+  paths: {
     'mathjax-stix2': FONTPATH
-  });
-  MathJax.config.loader['[tex]/ieeestix'] = {
+  },
+  '[tex]/ieeestix': {
     checkReady() {
-      return MathJax.loader.load(`[mathjax-stix2]/${MathJax.config?.startup?.output || 'chtml'}`);
+      const jax = MathJax.config.startup.output;
+      return MathJax.loader.load(`[mathjax-stix2]/${jax}.js`).then(() => {
+        MathJax.config.output.font = '[mathjax-stix2]';
+        Object.assign(MathJax.config[jax], {
+          dynamicPrefix: '[mathjax-stix2]/chtml/dynamic',
+          fontData: MathJax._.output.fonts['mathjax-stix2'][jax + '_ts'].MathJaxStix2Font
+        });
+        if (jax === 'chtml') {
+          Object.assign(MathJax.config[jax], {
+            fontURL: FONTPATH + '/chtml/woff',
+          });
+        }
+      });
     }
-  };
-}
+  }
+});
