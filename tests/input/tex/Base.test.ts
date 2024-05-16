2ec0da24bfc57698b6b17ec19e2a394da5fe5e38
zorkow <volker.sorge@gmail.com>
2024-05-16 16:13:15 +0200

    base package errors

--- tests/input/tex/Base.test.ts
+++ tests/input/tex/Base.test.ts
@@ -354,3 +354,171 @@ describe('DigitsEuropean', () => {
     "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\",\" display=\"block\">\n  <mo data-latex=\",\">,</mo>\n</math>"
   ));
 })
+
+describe('Error', () => {
+  it('Ampersand-error', () => toXmlMatch(tex2mml("&"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"&amp;\" display=\"block\">\n  <merror data-mjx-error=\"Misplaced &amp;\">\n    <mtext>Misplaced &amp;</mtext>\n  </merror>\n</math>"
+  ));
+  it('Argument-error', () => toXmlMatch(tex2mml("\\frac{b}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\frac{b}\" display=\"block\">\n  <merror data-mjx-error=\"Missing argument for \\frac\">\n    <mtext>Missing argument for \\frac</mtext>\n  </merror>\n</math>"
+  ));
+  it('Undefined-CS', () => toXmlMatch(tex2mml("\\nonsense"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\nonsense\" display=\"block\">\n  <merror data-mjx-error=\"Undefined control sequence \\nonsense\">\n    <mtext>Undefined control sequence \\nonsense</mtext>\n  </merror>\n</math>"
+  ));
+  it('Undefined-Env', () => toXmlMatch(tex2mml("\\begin{nonsense} a \\end{nonsense}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\begin{nonsense} a \\end{nonsense}\" display=\"block\">\n  <merror data-mjx-error=\"Unknown environment 'nonsense'\">\n    <mtext>Unknown environment 'nonsense'</mtext>\n  </merror>\n</math>"
+  ));
+  it('Double-super-error', () => toXmlMatch(tex2mml("x^2^3"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x^2^3\" display=\"block\">\n  <merror data-mjx-error=\"Double exponent: use braces to clarify\">\n    <mtext>Double exponent: use braces to clarify</mtext>\n  </merror>\n</math>"
+  ));
+  it('Double-over-error', () => toXmlMatch(tex2mml("\\sum^2^3"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sum^2^3\" display=\"block\">\n  <merror data-mjx-error=\"Double exponent: use braces to clarify\">\n    <mtext>Double exponent: use braces to clarify</mtext>\n  </merror>\n</math>"
+  ));
+  it('Limits Error', () => toXmlMatch(tex2mml("+\\limits^2"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"+\\limits^2\" display=\"block\">\n  <merror data-mjx-error=\"\\limits is allowed only on operators\">\n    <mtext>\\limits is allowed only on operators</mtext>\n  </merror>\n</math>"
+  ));
+  it('Double sub error', () => toXmlMatch(tex2mml("x_2_3"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x_2_3\" display=\"block\">\n  <merror data-mjx-error=\"Double subscripts: use braces to clarify\">\n    <mtext>Double subscripts: use braces to clarify</mtext>\n  </merror>\n</math>"
+  ));
+  it('Double under error', () => toXmlMatch(tex2mml("\\sum_2_3"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sum_2_3\" display=\"block\">\n  <merror data-mjx-error=\"Double subscripts: use braces to clarify\">\n    <mtext>Double subscripts: use braces to clarify</mtext>\n  </merror>\n</math>"
+  ));
+  it('Brace Superscript Error', () => toXmlMatch(tex2mml("x'^'"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x'^'\" display=\"block\">\n  <merror data-mjx-error=\"Missing open brace for superscript\">\n    <mtext>Missing open brace for superscript</mtext>\n  </merror>\n</math>"
+  ));
+  it('Double Prime Error', () => toXmlMatch(tex2mml("x^\\prime'"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x^\\prime'\" display=\"block\">\n  <merror data-mjx-error=\"Prime causes double exponent: use braces to clarify\">\n    <mtext>Prime causes double exponent: use braces to clarify</mtext>\n  </merror>\n</math>"
+  ));
+  it('Hash Error', () => toXmlMatch(tex2mml("#"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"#\" display=\"block\">\n  <merror data-mjx-error=\"You can't use 'macro parameter character #' in math mode\">\n    <mtext>You can't use 'macro parameter character #' in math mode</mtext>\n  </merror>\n</math>"
+  ));
+  it('Missing Right', () => toXmlMatch(tex2mml("\\left(\\middle|"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\left(\\middle|\" display=\"block\">\n  <merror data-mjx-error=\"Extra \\left or missing \\right\">\n    <mtext>Extra \\left or missing \\right</mtext>\n  </merror>\n</math>"
+  ));
+  it('Orphan Middle', () => toXmlMatch(tex2mml("\\middle|"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\middle|\" display=\"block\">\n  <merror data-mjx-error=\"Extra \\middle\">\n    <mtext>Extra \\middle</mtext>\n  </merror>\n</math>"
+  ));
+  it('Middle with Right', () => toXmlMatch(tex2mml("\\middle|\\right)"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\middle|\\right)\" display=\"block\">\n  <merror data-mjx-error=\"Extra \\middle\">\n    <mtext>Extra \\middle</mtext>\n  </merror>\n</math>"
+  ));
+  it('Misplaced Move Root', () => toXmlMatch(tex2mml("\\uproot{2}\\sqrt[3]{a}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\uproot{2}\\sqrt[3]{a}\" display=\"block\">\n  <merror data-mjx-error=\"\\uproot can appear only within a root\">\n    <mtext>\\uproot can appear only within a root</mtext>\n  </merror>\n</math>"
+  ));
+  it('Multiple Move Root', () => toXmlMatch(tex2mml("\\sqrt[\\uproot{-2}\\uproot{2}\\beta]{k}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sqrt[\\uproot{-2}\\uproot{2}\\beta]{k}\" display=\"block\">\n  <merror data-mjx-error=\"Multiple use of \\uproot\">\n    <mtext>Multiple use of \\uproot</mtext>\n  </merror>\n</math>"
+  ));
+  it('Incorrect Move Root', () => toXmlMatch(tex2mml("\\sqrt[\\uproot-2.5\\beta]{k}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sqrt[\\uproot-2.5\\beta]{k}\" display=\"block\">\n  <merror data-mjx-error=\"The argument to \\uproot must be an integer\">\n    <mtext>The argument to \\uproot must be an integer</mtext>\n  </merror>\n</math>"
+  ));
+  it('Double Over', () => toXmlMatch(tex2mml("1 \\over 2 \\over 3"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"1 \\over 2 \\over 3\" display=\"block\">\n  <merror data-mjx-error=\"Ambiguous use of \\over\">\n    <mtext>Ambiguous use of \\over</mtext>\n  </merror>\n</math>"
+  ));
+  it('Token Illegal Type', () => toXmlMatch(tex2mml("\\mmlToken{mk}[]{}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\mmlToken{mk}[]{}\" display=\"block\">\n  <merror data-mjx-error=\"mk is not a token element\">\n    <mtext>mk is not a token element</mtext>\n  </merror>\n</math>"
+  ));
+  it('Token Wrong Type', () => toXmlMatch(tex2mml("\\mmlToken{mrow}[]{}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\mmlToken{mrow}[]{}\" display=\"block\">\n  <merror data-mjx-error=\"mrow is not a token element\">\n    <mtext>mrow is not a token element</mtext>\n  </merror>\n</math>"
+  ));
+  it('Token Invalid Attribute', () => toXmlMatch(tex2mml("\\mmlToken{mi}[m1=true]{}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\mmlToken{mi}[m1=true]{}\" display=\"block\">\n  <merror data-mjx-error=\"Invalid MathML attribute: m1=true\">\n    <mtext>Invalid MathML attribute: m1=true</mtext>\n  </merror>\n</math>"
+  ));
+  it('Token Unknown Attribute', () => toXmlMatch(tex2mml("\\mmlToken{mo}[nothing=\"something\"]{}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\mmlToken{mo}[nothing=&quot;something&quot;]{}\" display=\"block\">\n  <merror data-mjx-error=\"nothing is not a recognized attribute for mo\">\n    <mtext>nothing is not a recognized attribute for mo</mtext>\n  </merror>\n</math>"
+  ));
+  it('Token Wrong Attribute', () => toXmlMatch(tex2mml("\\mmlToken{mi}[movablelimit=true]{}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\mmlToken{mi}[movablelimit=true]{}\" display=\"block\">\n  <merror data-mjx-error=\"movablelimit is not a recognized attribute for mi\">\n    <mtext>movablelimit is not a recognized attribute for mi</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingBeginExtraEnd', () => toXmlMatch(tex2mml("\\end{array}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\end{array}\" display=\"block\">\n  <merror data-mjx-error=\"Missing \\begin{array} or extra \\end{array}\">\n    <mtext>Missing \\begin{array} or extra \\end{array}</mtext>\n  </merror>\n</math>"
+  ));
+  it('ExtraCloseMissingOpen', () => toXmlMatch(tex2mml("x}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x}\" display=\"block\">\n  <merror data-mjx-error=\"Extra close brace or missing open brace\">\n    <mtext>Extra close brace or missing open brace</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingLeftExtraRight', () => toXmlMatch(tex2mml("x\\right\\}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x\\right\\}\" display=\"block\">\n  <merror data-mjx-error=\"Missing \\left or extra \\right\">\n    <mtext>Missing \\left or extra \\right</mtext>\n  </merror>\n</math>"
+  ));
+  it('ExtraOpenMissingClose', () => toXmlMatch(tex2mml("{x"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"{x\" display=\"block\">\n  <merror data-mjx-error=\"Extra open brace or missing close brace\">\n    <mtext>Extra open brace or missing close brace</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingScript Sub', () => toXmlMatch(tex2mml("x_"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x_\" display=\"block\">\n  <merror data-mjx-error=\"Missing superscript or subscript argument\">\n    <mtext>Missing superscript or subscript argument</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingScript Sup', () => toXmlMatch(tex2mml("x^"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x^\" display=\"block\">\n  <merror data-mjx-error=\"Missing superscript or subscript argument\">\n    <mtext>Missing superscript or subscript argument</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingOpenForSup', () => toXmlMatch(tex2mml("x^^"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x^^\" display=\"block\">\n  <merror data-mjx-error=\"Missing open brace for superscript\">\n    <mtext>Missing open brace for superscript</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingOpenForSub', () => toXmlMatch(tex2mml("x__"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"x__\" display=\"block\">\n  <merror data-mjx-error=\"Missing open brace for subscript\">\n    <mtext>Missing open brace for subscript</mtext>\n  </merror>\n</math>"
+  ));
+  it('ExtraLeftMissingRight', () => toXmlMatch(tex2mml("\\left\\{x"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\left\\{x\" display=\"block\">\n  <merror data-mjx-error=\"Extra \\left or missing \\right\">\n    <mtext>Extra \\left or missing \\right</mtext>\n  </merror>\n</math>"
+  ));
+  it('Misplaced Cr', () => toXmlMatch(tex2mml("a\\cr b"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"a\\cr b\" display=\"block\">\n  <merror data-mjx-error=\"Misplaced \\cr\">\n    <mtext>Misplaced \\cr</mtext>\n  </merror>\n</math>"
+  ));
+  it('Dimension Error', () => toXmlMatch(tex2mml("a\\\\[abc] b"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"a\\\\[abc] b\" display=\"block\">\n  <merror data-mjx-error=\"Bracket argument to \\\\ must be a dimension\">\n    <mtext>Bracket argument to \\\\ must be a dimension</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingArgFor', () => toXmlMatch(tex2mml("\\sqrt"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sqrt\" display=\"block\">\n  <merror data-mjx-error=\"Missing argument for \\sqrt\">\n    <mtext>Missing argument for \\sqrt</mtext>\n  </merror>\n</math>"
+  ));
+  it('ExtraCloseMissingOpen 2', () => toXmlMatch(tex2mml("\\sqrt}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sqrt}\" display=\"block\">\n  <merror data-mjx-error=\"Extra close brace or missing open brace\">\n    <mtext>Extra close brace or missing open brace</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingCloseBrace', () => toXmlMatch(tex2mml("\\sqrt{"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sqrt{\" display=\"block\">\n  <merror data-mjx-error=\"Missing close brace\">\n    <mtext>Missing close brace</mtext>\n  </merror>\n</math>"
+  ));
+  it('ExtraCloseLooking1', () => toXmlMatch(tex2mml("\\sqrt[3}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sqrt[3}\" display=\"block\">\n  <merror data-mjx-error=\"Extra close brace while looking for ']'\">\n    <mtext>Extra close brace while looking for ']'</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingCloseBracket', () => toXmlMatch(tex2mml("\\sqrt[3{x}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\sqrt[3{x}\" display=\"block\">\n  <merror data-mjx-error=\"Could not find closing ']' for argument to \\sqrt\">\n    <mtext>Could not find closing ']' for argument to \\sqrt</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingOrUnrecognizedDelim1', () => toXmlMatch(tex2mml("\\left\\alpha b"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\left\\alpha b\" display=\"block\">\n  <merror data-mjx-error=\"Missing or unrecognized delimiter for \\left\">\n    <mtext>Missing or unrecognized delimiter for \\left</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingOrUnrecognizedDelim2', () => toXmlMatch(tex2mml("\\left( b\\right"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\left( b\\right\" display=\"block\">\n  <merror data-mjx-error=\"Missing or unrecognized delimiter for \\right\">\n    <mtext>Missing or unrecognized delimiter for \\right</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingDimOrUnits', () => toXmlMatch(tex2mml("\\rule{}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\rule{}\" display=\"block\">\n  <merror data-mjx-error=\"Missing dimension or its units for \\rule\">\n    <mtext>Missing dimension or its units for \\rule</mtext>\n  </merror>\n</math>"
+  ));
+  it('TokenNotFoundForCommand', () => toXmlMatch(tex2mml("\\root {3] \\of 5"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\root {3] \\of 5\" display=\"block\">\n  <merror data-mjx-error=\"Could not find \\of for \\root\">\n    <mtext>Could not find \\of for \\root</mtext>\n  </merror>\n</math>"
+  ));
+  it('ExtraCloseLooking2', () => toXmlMatch(tex2mml("\\root [3} \\of 5 "),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\root [3} \\of 5 \" display=\"block\">\n  <merror data-mjx-error=\"Extra close brace while looking for \\of\">\n    <mtext>Extra close brace while looking for \\of</mtext>\n  </merror>\n</math>"
+  ));
+  it('ErroneousNestingEq', () => toXmlMatch(tex2mml("\\begin{equation}\\begin{eqnarray}\\end{eqnarray}\\end{equation}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\begin{equation}\\begin{eqnarray}\\end{eqnarray}\\end{equation}\" display=\"block\">\n  <merror data-mjx-error=\"Erroneous nesting of equation structures\">\n    <mtext>Erroneous nesting of equation structures</mtext>\n  </merror>\n</math>"
+  ));
+  it('ExtraAlignTab', () => toXmlMatch(tex2mml("\\cases{b & l & k}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\cases{b &amp; l &amp; k}\" display=\"block\">\n  <merror data-mjx-error=\"Extra alignment tab in \\cases text\">\n    <mtext>Extra alignment tab in \\cases text</mtext>\n  </merror>\n</math>"
+  ));
+  it('Misplaced hline', () => toXmlMatch(tex2mml("\\hline"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\hline\" display=\"block\">\n  <merror data-mjx-error=\"Misplaced \\hline\">\n    <mtext>Misplaced \\hline</mtext>\n  </merror>\n</math>"
+  ));
+  it('UnsupportedHFill', () => toXmlMatch(tex2mml("a\\hfill b"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"a\\hfill b\" display=\"block\">\n  <merror data-mjx-error=\"Unsupported use of \\hfill\">\n    <mtext>Unsupported use of \\hfill</mtext>\n  </merror>\n</math>"
+  ));
+  it('InvalidEnv', () => toXmlMatch(tex2mml("\\begin{\\ff}kk\\end{\\ff}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\begin{\\ff}kk\\end{\\ff}\" display=\"block\">\n  <merror data-mjx-error=\"Invalid environment name '\\ff'\">\n    <mtext>Invalid environment name '\\ff'</mtext>\n  </merror>\n</math>"
+  ));
+  it('EnvBadEnd', () => toXmlMatch(tex2mml("\\begin{equation}a\\end{array}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\begin{equation}a\\end{array}\" display=\"block\">\n  <merror data-mjx-error=\"\\begin{equation} ended with \\end{array}\">\n    <mtext>\\begin{equation} ended with \\end{array}</mtext>\n  </merror>\n</math>"
+  ));
+  it('EnvMissingEnd Array', () => toXmlMatch(tex2mml("\\begin{array}a"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\begin{array}a\" display=\"block\">\n  <merror data-mjx-error=\"Illegal pream-token (a)\">\n    <mtext>Illegal pream-token (a)</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingBoxFor', () => toXmlMatch(tex2mml("\\raise{2pt}"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\raise{2pt}\" display=\"block\">\n  <merror data-mjx-error=\"Missing box for \\raise\">\n    <mtext>Missing box for \\raise</mtext>\n  </merror>\n</math>"
+  ));
+  it('MissingCloseBrace2', () => toXmlMatch(tex2mml("\\begin{array}{c"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\begin{array}{c\" display=\"block\">\n  <merror data-mjx-error=\"Missing close brace\">\n    <mtext>Missing close brace</mtext>\n  </merror>\n</math>"
+  ));
+  it('EnvMissingEnd Equation', () => toXmlMatch(tex2mml("\\begin{equation}a"),
+    "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" data-latex=\"\\begin{equation}a\" display=\"block\">\n  <merror data-mjx-error=\"Missing \\end{equation}\">\n    <mtext>Missing \\end{equation}</mtext>\n  </merror>\n</math>"
+  ));
+})
