df536a2a19167a12691bed6106aa262f671aaa88
zorkow <volker.sorge@gmail.com>
2024-05-19 12:06:42 +0200

    introduce custom matcher to get useful match output

--- tests/src/xmlMatch.ts
+++ tests/src/xmlMatch.ts
@@ -1,6 +1,20 @@
 import { expect } from '@jest/globals';
 import { xml2json } from 'xml-js';
 
-export async function toXmlMatch(received: string, expected: string) {
-  expect(xml2json(received)).toStrictEqual(xml2json(expected));
+expect.extend({
+  // An xml matcher via deep equality on JSON objects.
+  toBeXmlMatch(received, expected) {
+    const recJson = xml2json(received);
+    const exptJson = xml2json(expected)
+    const pass = this.equals(recJson, exptJson, [this.utils.iterableEquality]);
+    const message = () =>
+      `Expected: ${this.utils.printExpected(expected)}\nReceived: ${this.utils.printReceived(received)}`;
+    return { pass, message }
+  },
+});
+
+export function toXmlMatch(received: string, expected: string) {
+  // This is slightly awkward way of getting around ts-jest problems with custom
+  // matcher extensions.
+  (expect(received) as any).toBeXmlMatch(expected);
 }
