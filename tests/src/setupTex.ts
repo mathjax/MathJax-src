342d9758bdacea2855595b17edd8e427e088068b
zorkow <volker.sorge@gmail.com>
2024-05-31 12:38:18 +0200

    remove extensions from imports in test sources

--- tests/src/setupTex.ts
+++ tests/src/setupTex.ts
@@ -1,10 +1,10 @@
-import {TeX} from '#js/input/tex.js';
-import {HTMLDocument} from '#js/handlers/html/HTMLDocument.js';
-import {liteAdaptor} from '#js/adaptors/liteAdaptor.js';
-import {STATE} from '#js/core/MathItem.js';
-import {SerializedMmlVisitor} from '#js/core/MmlTree/SerializedMmlVisitor.js';
-import {MmlNode} from '#js/core/MmlTree/MmlNode.js';
-import '#js/input/tex/AllPackages.js';
+import {TeX} from '#js/input/tex';
+import {HTMLDocument} from '#js/handlers/html/HTMLDocument';
+import {liteAdaptor} from '#js/adaptors/liteAdaptor';
+import {STATE} from '#js/core/MathItem';
+import {SerializedMmlVisitor} from '#js/core/MmlTree/SerializedMmlVisitor';
+import {MmlNode} from '#js/core/MmlTree/MmlNode';
+import '#js/input/tex/AllPackages';
 
 let convert: (tex: string) => string;
 
@@ -18,7 +18,7 @@ export function setupTex(packages: string[] = ['base'], options = {}) {
     toMathML(html.convert(expr, {display: true, end: STATE.CONVERT}));
 }
 
-import {SVG} from '#js/output/svg.js';
+import {SVG} from '#js/output/svg';
 
 export function setupTexWithOutput(packages: string[] = ['base'], options = {}) {
   const parserOptions = Object.assign({}, {packages: packages}, options);
