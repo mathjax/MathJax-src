a5eee416390bf7289baeb9c88bd676356a9d3dc4
zorkow <volker.sorge@gmail.com>
2025-05-13 01:48:35 +0200

    remove unused SRE API methods

--- ts/a11y/sre.ts
+++ ts/a11y/sre.ts
@@ -22,42 +22,31 @@
  * @author v.sorge@mathjax.org (Volker Sorge)
  */
 
-import * as Api from '#sre/common/system.js';
 import * as SpeechGeneratorFactory from '#sre/speech_generator/speech_generator_factory.js';
 import { Engine } from '#sre/common/engine.js';
 import { ClearspeakPreferences } from '#sre/speech_rules/clearspeak_preferences.js';
 import { Highlighter } from '#sre/highlighter/highlighter.js';
 import * as HighlighterFactory from '#sre/highlighter/highlighter_factory.js';
-import { SpeechGenerator } from '#sre/speech_generator/speech_generator.js';
-import { SemanticNode } from '#sre/semantic_tree/semantic_node.js';
 import { parseInput } from '#sre/common/dom_util.js';
 import { Variables } from '#sre/common/variables.js';
-
-export { semanticMathmlNode } from '#sre/enrich_mathml/enrich.js';
+import { semanticMathmlSync } from '#sre/enrich_mathml/enrich.js';
 
 export type highlighter = Highlighter;
 
-export type speechGenerator = SpeechGenerator;
-
-export type semanticNode = SemanticNode;
-
 export const locales = Variables.LOCALES;
 
-/**
- * A promise that resolves when SRE is loaded and ready, and rejects if
- * SRE can't be loaded, or does not become ready within the timout period.
- *
- * @deprecated
- */
-export const sreReady = Api.engineReady;
+export const setupEngine = (x: { [key: string]: string | boolean }) => {
+  return Engine.getInstance().setup(x);
+};
 
-export const setupEngine = Api.setupEngine;
+export const engineSetup = () => {
+  return Engine.getInstance().json();
+};
 
-export const engineSetup = Api.engineSetup;
-
-export const toEnriched = Api.toEnriched;
-
-export const toSpeech = Api.toSpeech;
+// export const toEnriched = Api.toEnriched;
+export const toEnriched = (mml: string) => {
+  return semanticMathmlSync(mml, Engine.getInstance().options);
+};
 
 export const clearspeakPreferences = ClearspeakPreferences;
 
@@ -68,21 +57,3 @@ export const updateHighlighter = HighlighterFactory.update;
 export const getSpeechGenerator = SpeechGeneratorFactory.generator;
 
 export const parseDOM = parseInput;
-
-/**
- * For now, we disable loading locales, since they are now only needed
- * in the web worker.
- *
- * @param {string} _locale A locale to preload from the bundle.
- * @returns {Promise} Promise that resolves when locales are loaded.
- */
-export const preloadLocales = async function (
-  _locale: string
-): Promise<string> {
-  return Promise.resolve('{}');
-};
-
-// Setting delay stops SRE from setting itself up (and loading locales) when it
-// is not actually being used. As we are not yet sure in which environment we
-// are (browser, node) we can not use a configuration vector.
-Engine.getInstance().delay = true;
