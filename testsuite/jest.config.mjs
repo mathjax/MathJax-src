685b5c0b9f57bbb4f414c874f678d764e998b308
zorkow <volker.sorge@gmail.com>
2025-06-04 09:22:20 +0200

    update test config for latest typescript and gh actions use

--- testsuite/jest.config.mjs
+++ testsuite/jest.config.mjs
@@ -3,20 +3,21 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+// import type {Config} from 'jest';
 import * as path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const tsjest = path.resolve(__dirname, 'node_modules', 'ts-jest');
 
-const config: Config = {
+const config = {
   rootDir: '..',
   clearMocks: true,
-  collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   coveragePathIgnorePatterns: ["node_modules", "testsuite", "mjs/util/entities"],
   testEnvironment: "node",
-  verbose: true,
   preset: tsjest,
   testMatch: [
     "**/tests/**/*.test.ts"
