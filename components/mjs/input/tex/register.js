c2675cf355ba004fb78da0d197502044a0b7bd43
Davide P. Cervone <dpvc@union.edu>
2025-07-30 13:58:31 -0400

    Fix registerTex() to handle tex.packages that is already an array

--- components/mjs/input/tex/register.js
+++ components/mjs/input/tex/register.js
@@ -12,6 +12,9 @@ export function registerTeX(packageList = [], tex = true) {
     let packages = MathJax.config.tex.packages;
     MathJax.config.tex.packages = packageList;
     if (packages) {
+      if (Array.isArray(packages)) {
+        packages = {'[+]': packages};
+      }
       insert(MathJax.config.tex, {packages});
     }
   }
