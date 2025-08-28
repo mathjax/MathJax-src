7545ae766dcb0161fa3796224de4cb40f01f053b
Davide P. Cervone <dpvc@union.edu>
2025-08-28 08:33:58 -0400

    Update tags in ams and mathtools packages

--- ts/input/tex/mathtools/MathtoolsTags.ts
+++ ts/input/tex/mathtools/MathtoolsTags.ts
@@ -104,9 +104,7 @@ export function MathtoolsTagFormat(
     public formatTag(tag: string) {
       if (this.mtCurrent) {
         const [left, right, format] = this.mtCurrent;
-        return format
-          ? `${left}${format}{${tag}}${right}`
-          : `${left}${tag}${right}`;
+        return format ? [left, `${format}{${tag}}`, right] : [left, tag, right];
       }
       return super.formatTag(tag);
     }
