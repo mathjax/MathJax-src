a4b9da1f0aa0ca26ce04f3acf5d820111fe4b20b
Davide P. Cervone <dpvc@union.edu>
2026-02-26 14:54:39 -0500

    Fix erroneous definition for --mjx-bg1-color.

--- ts/a11y/explorer/Region.ts
+++ ts/a11y/explorer/Region.ts
@@ -364,7 +364,7 @@ export class LiveRegion extends StringRegion {
       '--mjx-live-bg-color': 'white',
       '--mjx-live-shadow-color': '#888',
       '--mjx-live-border-color': '#CCCCCC',
-      '--mjx-bg1-color': 'rgba(var(--mjx-bg-blue), var(--mjx-bg-alpha))',
+      '--mjx-bg1-color': 'rgba(var(--mjx-bg-blue), var(--mjx-bg1-alpha))',
       '--mjx-fg1-color': 'rgba(var(--mjx-fg-black), 1)',
       '--mjx-bg2-color': 'rgba(var(--mjx-bg-red), 1)',
       '--mjx-fg2-color': 'rgba(var(--mjx-fg-black), 1)',
