56afc42719b20b7fea305c7acb7be4b39f36d98c
zorkow <volker.sorge@gmail.com>
2026-03-12 11:27:08 +0100

    rewrite bbox tests to snapshots

--- testsuite/tests/input/tex/Bbox.test.ts
+++ testsuite/tests/input/tex/Bbox.test.ts
@@ -1,5 +1,5 @@
-import { afterAll, beforeEach, describe, it } from '@jest/globals';
-import { getTokens, toXmlMatch, setupTex, tex2mml, expectTexError } from '#helpers';
+import { afterAll, beforeEach, describe, expect, it } from '@jest/globals';
+import { getTokens, setupTex, tex2mml, expectTexError } from '#helpers';
 import '#js/input/tex/bbox/BboxConfiguration';
 
 beforeEach(() => setupTex(['base', 'bbox']));
@@ -12,85 +12,37 @@ describe('Bbox', () => {
   /********************************************************************************/
 
   it('Bbox-Background', () => {
-    toXmlMatch(
-      tex2mml('\\bbox[yellow]{a}'),
-      `<math xmlns="http://www.w3.org/1998/Math/MathML" data-latex="\\bbox[yellow]{a}" display="block">
-         <mstyle mathbackground="yellow" data-latex="\\bbox[yellow]{a}">
-           <mi data-latex="a">a</mi>
-         </mstyle>
-       </math>`
-    );
+    expect(tex2mml('\\bbox[yellow]{a}')).toMatchSnapshot();
   });
 
   /********************************************************************************/
 
   it('Bbox-Padding', () => {
-    toXmlMatch(
-      tex2mml('\\bbox[5px]{a}'),
-      `<math xmlns="http://www.w3.org/1998/Math/MathML" data-latex="\\bbox[5px]{a}" display="block">
-         <mpadded height="+5px" depth="+5px" lspace="5px" width="+10px" data-latex="\\bbox[5px]{a}">
-           <mi data-latex="a">a</mi>
-         </mpadded>
-       </math>`
-    );
+    expect(tex2mml('\\bbox[5px]{a}')).toMatchSnapshot();
   });
 
   /********************************************************************************/
 
   it('Bbox-Frame', () => {
-    toXmlMatch(
-      tex2mml('\\bbox[border:5px solid red]{a}'),
-      `<math xmlns="http://www.w3.org/1998/Math/MathML" data-latex="\\bbox[border:5px solid red]{a}" display="block">
-         <mstyle style="border:5px solid red" data-latex="\\bbox[border:5px solid red]{a}">
-           <mi data-latex="a">a</mi>
-         </mstyle>
-       </math>`
-    );
+    expect(tex2mml('\\bbox[border:5px solid red]{a}')).toMatchSnapshot();
   });
 
   /********************************************************************************/
 
   it('Bbox-Background-Padding', () => {
-    toXmlMatch(
-      tex2mml('\\bbox[yellow,5px]{a}'),
-      `<math xmlns="http://www.w3.org/1998/Math/MathML" data-latex="\\bbox[yellow,5px]{a}" display="block">
-         <mstyle mathbackground="yellow" data-latex="\\bbox[yellow,5px]{a}">
-           <mpadded height="+5px" depth="+5px" lspace="5px" width="+10px">
-             <mi data-latex="a">a</mi>
-           </mpadded>
-         </mstyle>
-       </math>`
-    );
+    expect(tex2mml('\\bbox[yellow,5px]{a}')).toMatchSnapshot();
   });
 
   /********************************************************************************/
 
   it('Bbox-Padding-Frame', () => {
-    toXmlMatch(
-      tex2mml('\\bbox[5px,border:2px solid red]{a}'),
-      `<math xmlns="http://www.w3.org/1998/Math/MathML" data-latex="\\bbox[5px,border:2px solid red]{a}" display="block">
-         <mstyle style="border:2px solid red" data-latex="\\bbox[5px,border:2px solid red]{a}">
-           <mpadded height="+5px" depth="+5px" lspace="5px" width="+10px">
-             <mi data-latex="a">a</mi>
-           </mpadded>
-         </mstyle>
-       </math>`
-    );
+    expect(tex2mml('\\bbox[5px,border:2px solid red]{a}')).toMatchSnapshot();
   });
 
   /********************************************************************************/
 
   it('Bbox-Background-Padding-Frame', () => {
-    toXmlMatch(
-      tex2mml('\\bbox[yellow,5px,border:2px solid red]{a}'),
-      `<math xmlns="http://www.w3.org/1998/Math/MathML" data-latex="\\bbox[yellow,5px,border:2px solid red]{a}" display="block">
-         <mstyle mathbackground="yellow" style="border:2px solid red" data-latex="\\bbox[yellow,5px,border:2px solid red]{a}">
-           <mpadded height="+5px" depth="+5px" lspace="5px" width="+10px">
-             <mi data-latex="a">a</mi>
-           </mpadded>
-         </mstyle>
-       </math>`
-    );
+    expect(tex2mml('\\bbox[yellow,5px,border:2px solid red]{a}')).toMatchSnapshot();
   });
 
   /********************************************************************************/
