d09b67941f6c5d964487baddf80be0e90397cc38
Davide P. Cervone <dpvc@union.edu>
2026-05-30 16:09:28 -0400

    Localize a11y/semantic-enrich

--- ts/a11y/semantic-enrich.ts
+++ ts/a11y/semantic-enrich.ts
@@ -39,6 +39,8 @@ import { MathML } from '../input/mathml.js';
 import { SerializedMmlVisitor } from '../core/MmlTree/SerializedMmlVisitor.js';
 import { OptionList, expandable } from '../util/Options.js';
 import * as Sre from './sre.js';
+import { Locale } from '../util/Locale.js';
+import { COMPONENT } from './semantic-enrich/__locales__/Component.js';
 
 /*==========================================================================*/
 
@@ -399,7 +401,7 @@ export function EnrichedMathDocumentMixin<
       _math: EnrichedMathItem<N, T, D>,
       err: Error
     ) {
-      console.warn('Enrichment error:', err);
+      console.warn(Locale.message(COMPONENT, 'EnrichError'), err);
     }
 
     /**
