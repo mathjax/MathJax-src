67cfb3ba328affd9c570576283e700de217946bc
zorkow <volker.sorge@gmail.com>
2026-06-08 15:21:16 +0200

    add as separate components

---
+++ components/mjs/a11y/en.js
@@ -0,0 +1,5 @@
+import { Locale } from '#js/util/Locale.js';
+import { COMPONENT } from '#js/a11y/__locales__/Component.js';
+import data from '#ts/a11y/__locales__/en.json' with {type: 'json'};
+
+Locale.registerMessages(COMPONENT, 'en', data);
