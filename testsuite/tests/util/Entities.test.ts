213d62fe554dcccc7728d4c684dfe9787781808d
Davide P. Cervone <dpvc@union.edu>
2026-07-22 13:42:41 -0400

    Allow entity definitions to load synchronously, and add tests for it.

--- testsuite/tests/util/Entities.test.ts
+++ testsuite/tests/util/Entities.test.ts
@@ -1,25 +1,64 @@
 import { describe, test, expect } from '@jest/globals';
 import * as Entities from '#js/util/Entities.js';
-import { handleRetriesFor } from '#js/util/Retries.js';
+import { mathjax } from '#js/mathjax.js';
 import '#js/util/asyncLoad/esm.js';
+import '#js/a11y/sre/require.mjs';
 
 describe('Entities translation', () => {
-  test('translate()', async () => {
+  test('translate()', () => {
     expect(Entities.translate('&#97;')).toBe('a');
     expect(Entities.translate('&#x61;')).toBe('a');
     expect(Entities.translate('&amp;')).toBe('&');
+  });
+
+  test('Unknown entity', async () => {
     await expect(
-      handleRetriesFor(() => Entities.translate('&xyz;'))
+      mathjax.handleRetriesFor(() => Entities.translate('&xyz;'))
     ).resolves.toBe('&xyz;'); // no such entity
+  });
+
+  test('Load entity files', async () => {
     await expect(
-      handleRetriesFor(() => Entities.translate('&approx;'))
+      mathjax.handleRetriesFor(() => Entities.translate('&approx;'))
     ).resolves.toBe('\u2248'); // load a.js
     await expect(
-      handleRetriesFor(() => Entities.translate('&Bscr;'))
+      mathjax.handleRetriesFor(() => Entities.translate('&Bscr;'))
     ).resolves.toBe('\u212C'); // load scr.js
-    Entities.remove('approx');
-    expect(Entities.translate('&approx;')).toBe('&approx;'); // undefined entities remain unchanged
     Entities.options.loadMissingEntities = false;
     expect(Entities.translate('&bigwedge;')).toBe('&bigwedge;'); // don't load b.js
+    Entities.options.loadMissingEntities = true;
+  });
+
+  test('Remove entity', () => {
+    Entities.remove('approx');
+    expect(Entities.translate('&approx;')).toBe('&approx;'); // undefined entities remain unchanged
+  });
+
+  test('Synchronous load', () => {
+    const asyncLoad = mathjax.asyncLoad;
+    const REQUIRE = require;
+    mathjax.asyncIsSynchronous = true;
+    mathjax.asyncLoad = (file) => REQUIRE(`#js/../cjs/${file}`);
+    expect((() => {
+      try {
+        Entities.translate('&copy;');
+        return 'success';
+      } catch (_err) {
+        return 'failed';
+      }
+    })()).toBe('success');
+    mathjax.asyncLoad = asyncLoad;
+    mathjax.asyncIsSynchronous = false;
+  });
+
+  test('AsycLoad retries', () => {
+    expect((() => {
+      try {
+        Entities.translate('&divide;');
+        return 'success';
+      } catch (_err) {
+        return 'failed';
+      }
+    })()).toBe('failed');
   });
 });
