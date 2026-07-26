6fd363ca209c8f16c9e854e26a947f27887c586d
Davide P. Cervone <dpvc@union.edu>
2026-07-26 10:22:59 -0400

    Make sure SRE options are set if only a convert call is used.

--- ts/a11y/semantic-enrich.ts
+++ ts/a11y/semantic-enrich.ts
@@ -202,6 +202,9 @@ export function EnrichedMathItemMixin<
           } else {
             mml = this.adjustSelections();
           }
+          if (!document.processed.isSet('enriched')) {
+            Sre.setupEngine(document.options.sre);
+          }
           const enriched = Sre.toEnriched(mml);
           this.inputData.enrichedMml = math.math = this.serializeMml(enriched);
           //
@@ -351,7 +354,7 @@ export function EnrichedMathDocumentMixin<
 
     /**
      * Enrich the MathItem class used for this MathDocument, and create the
-     *   temporary MathItem used for enrchment
+     *   temporary MathItem used for enrichment
      *
      * @override
      * @class
